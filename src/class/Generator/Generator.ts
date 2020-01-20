b9c90ad97865f5daed5555779394315bd1f48f7e
pieaces <jaesung446@gmai.com>
2020-01-20 19:13:58 +0900

    v 1.1.4 Generator 구현

--- src/class/Generator/Generator.ts
+++ src/class/Generator/Generator.ts
@@ -1,22 +1,22 @@
 import Calculate from '../Statistics/Calculate'
 /*
-전멸갯수(0~4) 입력 AnnihilatedLine
-제외할 공색(0,1,2,3,4) 중 택(전멸갯수만큼)
-십의자리 합(0~24) 입력
-홀수갯수(0~6)
-3의배수 갯수(0~6)
-AC값(0~10)
-고저차(5~44)
+전멸갯수(0~4) 정확히 입력
+제외할 공색(0,1,2,3,4) 중 택(전멸갯수만큼) 정확히
+십의자리 합(0~24) 입력 범위 
+홀수갯수(0~6) 정확히
+3의배수 갯수(0~6) 정확히
+AC값(0~10) 범위
+고저차(5~44) 범위
 이월갯수(0~6)
 */
 class Generator {
     annihilatedLineCount: number;
     exceptedLine: number[];
-    sum$10: number;
+    sum$10: [number, number];
     oddCount: number;
     $3Count: number;
-    ac: number;
-    diffMaxMin: number;
+    ac: [number, number];
+    diffMaxMin: [number, number];
     carryCount?: number;
 
     traversal(method?: () => void): number {
@@ -41,7 +41,7 @@ class Generator {
             });
             const result: number[] = [];
             temp.forEach((value, index) => {
-                if (value) result.push(index);
+                if (!value) result.push(index);
             });
 
             return result;
@@ -53,7 +53,6 @@ class Generator {
 
             const exceptedLines = exceptedLine(box);
             if (Calculate.annihilatedLineCount(box) !== this.annihilatedLineCount) {
-                console.log('전멸갯수');
                 check = false;
             }
             /*
@@ -64,37 +63,30 @@ class Generator {
             }*/
             exceptedLines.forEach((value, i) => {
                 if (value !== this.exceptedLine[i]) {
-                    console.log(box, '전멸라인');
                     check = false;
                 }
             });
-            if (Calculate.sum$10(box) !== this.sum$10) {
-                console.log(box, '십의자리합');
-
+            if (!(Calculate.sum$10(box) <= this.sum$10[0] && Calculate.sum$10(box) <= this.sum$10[1])) {
                 check = false;
             }
             if (Calculate.oddCount(box) !== this.oddCount) {
-                console.log(box, '홀수개수');
 
                 check = false;
             }
             if (Calculate.$3Count(box) !== this.$3Count) {
-                console.log(box, '3배수개수');
 
                 check = false;
             }
-            if (Calculate.AC(box) !== this.ac) {
-                console.log(box, 'AC합');
+            if (!(Calculate.AC(box) <= this.ac[0] && Calculate.AC(box) <= this.ac[1])) {
 
                 check = false;
             }
-            if (Calculate.diffMaxMin(box) !== this.diffMaxMin) {
-                console.log(box, '고저차');
+            if (!(Calculate.diffMaxMin(box) <= this.diffMaxMin[0] && Calculate.diffMaxMin(box) <= this.diffMaxMin[1])) {
+
                 check = false;
             }
             //
             if (check) {
-                console.log(count);
                 count++;
             }
             if (box[0] === upb[0]) break;
@@ -110,10 +102,10 @@ class Generator {
 const gen = new Generator();
 gen.annihilatedLineCount = 1;
 gen.exceptedLine = [2];
-gen.sum$10 = 140;
+gen.sum$10 = [11,12];
 gen.oddCount = 3;
 gen.$3Count = 2;
-gen.ac = 7;
-gen.diffMaxMin = 12;
+gen.ac = [7,9];
+gen.diffMaxMin = [35,39];
 
 console.log(gen.traversal());
