9a3579df50ea81c127e4ceb01642ad51a90693d8
pieaces <jaesung446@gmail.com>
2020-02-04 22:17:16 +0900

    current

--- LottoAPI/app.ts
+++ LottoAPI/app.ts
@@ -72,39 +72,39 @@ app.post('/numbers/generator', (req, res) => {
         option.consecutiveExist = req.body.consecutiveExist;
     }
 
-    const current: string = req.body.current;
+    const current: Method = req.body.current;
     const generator = new Generator(option);
 
     switch (current) {
-        case 'excludedLines':
+        case Method.excludedLineCount:
             res.json({
                 range: generator.constraintLowCount()
             });
             break;
-        case 'lowCount':
+        case Method.lowCount:
             res.json({
                 range: generator.constraintSum()
             });
             break;
-        case 'sum':
+        case Method.sum:
             generator.rangeFinder = Calculate.oddCount;
             break;
-        case 'oddCount':
+        case Method.oddCount:
             generator.rangeFinder = Calculate.primeCount;
             break;
-        case 'primeCount':
+        case Method.primeCount:
             generator.rangeFinder = Calculate.$3Count;
             break;
-        case '$3Count':
+        case Method.$3Count:
             generator.rangeFinder = Calculate.sum$10;
             break;
-        case 'sum$10':
+        case Method.sum$10:
             generator.rangeFinder = Calculate.diffMaxMin;
             break;
-        case 'diffMaxMin':
+        case Method.diffMaxMin:
             generator.rangeFinder = Calculate.AC;
             break;
-        case 'AC':
+        case Method.AC:
             generator.rangeFinder = Calculate.consecutiveExist;
             break;
     }
