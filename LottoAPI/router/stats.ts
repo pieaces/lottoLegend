f74449c1d09b93ad063245626bbebfa60728e804
pieaces <jaesung446@gmai.com>
2020-02-06 13:51:28 +0900

    list decodeURI로 추가

--- LottoAPI/router/stats.ts
+++ LottoAPI/router/stats.ts
@@ -9,13 +9,14 @@ router.get('/:method', async (req, res) => {
         res.json("The parameter doesn't exist");
     }
     const {from, to } = req.query;
-
+    const list = JSON.parse(decodeURI(req.query.list));
+    
     const theDate = new Date('2020-02-01:20:40');
     const today = new Date();
     const between = Number(today) - Number(theDate);
     const plusDate = Math.floor(between / 24 / 3600 / 1000 / 7);
 
-    const data = await queryStats(method, {from, to});
+    const data = await queryStats(method, {from, to, list});
     
     res.json({ data, total: 896 + plusDate });
 });
