5d9cec0082eb9fa8c5beb1f8b24776ecaf52d984
sso <sso9524@naver.com>
2020-02-06 18:32:47 +0900

    파일 쪼개기

--- front-end/js/lotto-func1/chartDesign.js
+++ front-end/js/lotto-func1/chartDesign.js
@@ -1,9 +1,3 @@
-const lottofunc1 = {
-  lottoData: {
-    oddCount: null
-  }
-};
-
 function filterBoxCheck() {
   const filterBox = document.querySelector('.filter-box');
   const filterArrow = document.querySelector('.filter-arrow');
@@ -407,28 +401,3 @@ function chartInit() {
 
   chartSlide(chartBarSlideObj);
 }
-
-async function setLottoOddCount() {
-  let response = await getLottoOddCount();
-  lottofunc1.lottoData.oddCount = response;
-}
-
-async function getLottoOddCount() {
-  const headers = {
-    'x-api-key': 'LZn9Pykicg982PNmTmdiB8pkso4xbGiQ4n4P1z1k' //API KEY
-  };
-  const fetchResult = await fetch(
-    'https://is6q0wtgml.execute-api.ap-northeast-2.amazonaws.com/dev/stats/oddCount?from=0&to=6', //API 주소, querystring = name
-    { method: 'GET', headers }
-  );
-  const data = JSON.parse(await fetchResult.text());
-  return data;
-}
-
-async function init() {
-  filterBoxCheck();
-  await setLottoOddCount();
-  chartInit();
-}
-
-init();
