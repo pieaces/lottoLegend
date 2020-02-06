523ffc3f97125ad72b251b83b32c6bff85f25850
pieaces <jaesung446@gmai.com>
2020-02-06 14:46:05 +0900

    API method 추가(excludeInclude)

--- LottoAPI/router/stats.ts
+++ LottoAPI/router/stats.ts
@@ -4,25 +4,37 @@ import { Method, QueryStatsParams } from '../interface/LottoDB';
 const router = express.Router();
 
 router.get('/:method', async (req, res) => {
-    const method: Method = req.params.method as Method;
-    if (!(method in Method)) {
-        res.json("The parameter doesn't exist");
+    const method = req.params.method;
+    let data;
+    if (method in Method) {
+
+        const temp: QueryStatsParams = {}
+        if (req.query.list) {
+            temp.list = JSON.parse(decodeURI(req.query.list));
+        } else if (req.query.from && req.query.to) {
+            temp.from = req.query.from;
+            temp.to = req.query.to;
+        }
+        data = await queryStats(method as Method, temp);
     }
-    const temp:QueryStatsParams = {}
-    if(req.query.list){
-        temp.list = JSON.parse(decodeURI(req.query.list));
-    }else if(req.query.from && req.query.to){
-        temp.from = req.query.from;
-        temp.to = req.query.to;
+    else {
+        if (method === "excludeInclude") {
+            let temp:any = {};
+            temp.emergence = await queryStats("emergence" as Method, {});
+            temp.interval = await queryStats("interval" as Method, {});
+            temp.howLongNone = await queryStats("howLongNone" as Method, {});
+            temp.frequency = await queryStats("frequency" as Method, {});
+
+            data = temp;
+        }
+        else res.json('wrong method');
     }
-    
+
     const theDate = new Date('2020-02-01:20:40');
     const today = new Date();
     const between = Number(today) - Number(theDate);
     const plusDate = Math.floor(between / 24 / 3600 / 1000 / 7);
 
-    const data = await queryStats(method, temp);
-    
     res.json({ data, total: 896 + plusDate });
 });
 
