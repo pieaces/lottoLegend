59621a51057cea5c0e910dae47e697d50dd549d9
pieaces <jaesung446@gmai.com>
2020-02-07 11:48:00 +0900

    generator API current삭제

--- LottoAPI/router/numbers.ts
+++ LottoAPI/router/numbers.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import Generator from '../class/Generator';
 import Calculate from '../class/Calculate';
-import { Method } from '../interface/LottoDB';
 import { GeneratorOption } from '../interface/Generator';
 const router = express.Router();
 
 router.post('/generator', (req, res) => {
     const option: GeneratorOption = {};
+    let willRangeFinder: (numbers: number[])=> number = null;
 
     if (req.body.excludedLines) {
         option.excludedLines = req.body.excludedLines;
@@ -22,67 +22,44 @@ router.post('/generator', (req, res) => {
     }
     if (req.body.sum) {
         option.sum = req.body.sum;
+        willRangeFinder = Calculate.oddCount;
     }
     if (req.body.oddCount) {
         option.oddCount = req.body.oddCount;
+        willRangeFinder = Calculate.primeCount;
     }
     if (req.body.primeCount) {
         option.primeCount = req.body.primeCount;
+        willRangeFinder = Calculate.$3Count;
     }
     if (req.body.$3Count) {
         option.$3Count = req.body.$3Count;
+        willRangeFinder = Calculate.sum$10;
     }
     if (req.body.sum$10) {
         option.sum$10 = req.body.sum$10;
+        willRangeFinder = Calculate.diffMaxMin;
     }
     if (req.body.diffMaxMin) {
         option.diffMaxMin = req.body.diffMaxMin;
+        willRangeFinder = Calculate.AC;
     }
     if (req.body.AC) {
         option.AC = req.body.AC;
+        willRangeFinder = Calculate.consecutiveExist;
     }
     if (typeof req.body.consecutiveExist !== 'undefined') {
         option.consecutiveExist = req.body.consecutiveExist;
     }
 
-    const current: Method = req.body.current;
     const generator = new Generator(option);
-
-    switch (current) {
-        case Method.excludedLineCount:
-            res.json({
-                range: generator.constraintLowCount()
-            });
-            break;
-        case Method.lowCount:
-            res.json({
-                range: generator.constraintSum()
-            });
-            break;
-        case Method.sum:
-            generator.rangeFinder = Calculate.oddCount;
-            break;
-        case Method.oddCount:
-            generator.rangeFinder = Calculate.primeCount;
-            break;
-        case Method.primeCount:
-            generator.rangeFinder = Calculate.$3Count;
-            break;
-        case Method.$3Count:
-            generator.rangeFinder = Calculate.sum$10;
-            break;
-        case Method.sum$10:
-            generator.rangeFinder = Calculate.diffMaxMin;
-            break;
-        case Method.diffMaxMin:
-            generator.rangeFinder = Calculate.AC;
-            break;
-        case Method.AC:
-            generator.rangeFinder = Calculate.consecutiveExist;
-            break;
-    }
+    generator.rangeFinder = willRangeFinder;
     generator.generate();
-    if (current === 'consecutiveExist') {
+
+    if(!generator.option.sum){
+        res.json("Not Proper Parameters");
+    }
+    else if (typeof generator.option.consecutiveExist !== 'undefined') {
         res.json({
             numbers: generator.getGeneratedNumbers()
         });
