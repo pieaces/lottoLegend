8da486c266dca7dd6bfbfdb40c1ddf3dc05786d0
pieaces <jaesung446@gmail.com>
2020-02-10 22:20:20 +0900

    radarSlide makeclickable

--- front-end/functional/instance2/radarSlide.ts
+++ front-end/functional/instance2/radarSlide.ts
@@ -6,6 +6,17 @@ const numBtns = document.querySelectorAll('.fun2-radar-nums-btn');
 const textBox = document.querySelector<HTMLElement>('.func2-chart-radar-text');
 
 const radarSlide = new RadarSlide(radarInstance, null, null, numBtns, textBox);
-makeClickable(radarSlide);
+const clickEvent = {
+    numBtns:
+        [() => {
+            radarInstance.canvas.style.transform = 'translateX(-100%)';
+            textBox.style.transform = 'translateX(-100%)';
+        },
+        () => {
+            radarInstance.canvas.style.transform = '';
+            textBox.style.transform = '';
+        }]
+}
+makeClickable(radarSlide, clickEvent);
 
 export default radarSlide;
