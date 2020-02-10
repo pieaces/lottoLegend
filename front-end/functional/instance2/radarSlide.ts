3d0c80617f609d9fd8a818160c1f5a8e3751b7a6
pieaces <jaesung446@gmail.com>
2020-02-10 22:30:26 +0900

    radarSlide element이름 변경 - 상쓰보이의 부탁

--- front-end/functional/instance2/radarSlide.ts
+++ front-end/functional/instance2/radarSlide.ts
@@ -2,10 +2,10 @@ import RadarSlide from "../Slide/radarSlide";
 import radarInstance from "./radarInstance";
 import makeClickable from "../Slide/makeClickable";
 
-const numBtns = document.querySelectorAll('.fun2-radar-nums-btn');
+const radarBtns = document.querySelectorAll('.fun2-chart-radar-num');
 const textBox = document.querySelector<HTMLElement>('.func2-chart-radar-text');
 
-const radarSlide = new RadarSlide(radarInstance, null, null, numBtns, textBox);
+const radarSlide = new RadarSlide(radarInstance, null, null, radarBtns, textBox);
 const clickEvent = {
     numBtns:
         [() => {
