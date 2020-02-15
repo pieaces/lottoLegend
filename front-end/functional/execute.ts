a88df0deb261c4b81b1d98d5b4ddc535e2ca23f6
pieaces <jaesung446@gmail.com>
2020-02-15 19:54:19 +0900

    loading 첫화면에 뜨기

--- front-end/functional/execute.ts
+++ front-end/functional/execute.ts
@@ -1,9 +1,12 @@
 import DataAPI from "./DataAPI";
 import Layout from './Layout'
 
+const loading = document.querySelector<HTMLElement>('.loading');
 const layout = new Layout();
 async function execute() {
+    loading.classList.remove('none');
     await DataAPI.getInstance().init();
     layout.init();
+    loading.classList.add('none');
 }
 execute();
