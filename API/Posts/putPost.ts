bc35fe28fd05fa0d8f4ee7a70a37d20b4eeabcdc
pieaces <jaesung446@gmail.com>
2020-02-17 17:58:32 +0900

    putPost.ts 생성

---
+++ API/Posts/putPost.ts
@@ -0,0 +1,48 @@
+import AWS from 'aws-sdk';
+import uuidv from 'uuid/v1';
+
+// AWS.config.update(require('./key.json'));
+const dynamodb = new AWS.DynamoDB();
+
+export interface Post {
+    title: string;
+    contents: string;
+    writerId: string;
+    writerName: string;
+}
+export default async function putPost(post: Post): Promise<boolean> {
+    const d = new Date();
+    const params = {
+        Item: {
+            "Id": {
+                S: uuidv()
+            },
+            "Title": {
+                S: post.title
+            },
+            "WriterId": {
+                S: post.writerId
+            },
+            "WriterName": {
+                S: post.writerName
+            },
+            "Contents": {
+                S: post.contents
+            },
+            "ReportingDate": {
+                S: `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()} ${d.getHours()}:${d.getMinutes()}`
+            }
+        },
+        TableName: "Posts"
+    };
+    return await new Promise((resolve) => {
+        dynamodb.putItem(params, function (err, data) {
+            if (err) {
+                console.log('putPost 에러: ', err);
+                resolve(false);
+            } else {
+                resolve(true);
+            }
+        });
+    });
+}
--- API/router/posts.ts
+++ API/router/posts.ts
@@ -1,8 +1,16 @@
 import express from 'express';
+import putPost, { Post } from '../Posts/putPost'
+
 const router = express.Router();
-
-router.get('/', async (req, res) => {
-
+router.post('/', async (req, res) => {
+    const post: Post = {
+        title: req.body.title,
+        writerId: 'devId',
+        writerName: 'devName',
+        contents: req.body.contents
+    };
+    const bool = await putPost(post);
+    res.json(bool);
 });
 
 export default router;
