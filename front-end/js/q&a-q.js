c26c5e98712ad659240fc5b1d4d9c7c91931e94e
sso <sso9524@naver.com>
2020-02-17 15:26:12 +0900

    q&a 페이지 작성중

--- front-end/js/q&a-q.js
+++ front-end/js/q&a-q.js
@@ -2,10 +2,18 @@
  * ID : 'suneditor_sample'
  * ClassName : 'sun-eidtor'
  */
+
+import SUNEDITOR from 'suneditor'
+
+import image from 'suneditor/src/plugins/dialog/link'
 // ID or DOM object
 const editor = SUNEDITOR.create(document.getElementById("sample") || "sample", {
   // All of the plugins are loaded in the "window.SUNEDITOR" object in dist/suneditor.min.js file
   // Insert options
+  plugins: [image],
+  buttonList: [
+    ['image']
+  ],
   width: "100%",
   height: "300",
   maxHeight: "300",
--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,48 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
 const path = require('path');
 
+// module.exports = {
+//     mode: 'development',
+//     entry: './front-end/functional/execute.ts',
+//     output: {
+//         filename: 'functional.js',
+//         path: path.resolve(__dirname, 'front-end')
+//     },
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.ts$/,
+//                 use: ['ts-loader'],
+//                 exclude: /node_modules/
+//             }
+//         ]
+//     },
+//     devServer: {
+//         contentBase: './front-end',
+//         overlay: true,
+//         hot: true
+//     },
+//     plugins: [
+//         new webpack.HotModuleReplacementPlugin(),
+//         new CopyWebpackPlugin(['./front-end/function.html']),
+
+//     ],
+//     resolve: {
+//         extensions: ['.ts', '.js', '.json']
+//     }
+// };
+
 module.exports = {
     mode: 'development',
-    entry: './front-end/functional/execute.ts',
+    entry: './front-end/js/q&a-q.js',
     output: {
-        filename: 'functional.js',
+        filename: 'q&a-q.js',
         path: path.resolve(__dirname, 'front-end')
     },
     module: {
         rules: [
             {
-                test: /\.ts$/,
+                test: /\.js$/,
                 use: ['ts-loader'],
                 exclude: /node_modules/
             }
@@ -25,7 +56,7 @@ module.exports = {
     },
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
-        new CopyWebpackPlugin(['./front-end/function.html']),
+        new CopyWebpackPlugin(['./front-end/q&a-q.html']),
 
     ],
     resolve: {
