af3971aaad4aae841e4727225888b8981db30dd0
pieaces <jaesung446@gmai.com>
2020-02-18 21:10:57 +0900

    db lambda에 적합하도록 변경

--- API/index.ts
+++ API/index.ts
@@ -1,3 +1,39 @@
-exports.handler = (event:any, context:any) => {
-  console.log(`EVENT: ${JSON.stringify(event)}`);
+import Posts from "./DB/Posts";
+
+exports.handler = async (event: any, context: any, callback: any) => {
+    const method = event.httpMethod;
+    const resource = event.resource;
+
+    let statusCode = 200;
+    const headers = {
+        "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+        //"Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
+    }
+    let body: any;
+
+    const db = new Posts();
+    if (resource === '/posts') {
+        switch (method) {
+            case 'GET':
+                const posts = await db.scan();
+                body = posts;
+                break;
+            case 'POST':
+                const { title, writerId, writerName, contents } = event.body;
+                const insertId = await db.put(title, writerId, writerName, contents);
+                body = insertId;
+                break;
+            default:
+                statusCode = 400;
+        }
+    } else {
+        statusCode = 400;
+    }
+    const response = {
+        statusCode,
+        headers,
+        body: JSON.stringify(body),
+    };
+    return response;
 };
+    //if (event.queryStringParameters && event.queryStringParameters.name) name = event.queryStringParameters.name;
