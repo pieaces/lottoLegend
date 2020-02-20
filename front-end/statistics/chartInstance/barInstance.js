7a31ab88263822f36561807bac55eb7de32339f5
sso <sso9524@naver.com>
2020-02-20 23:56:27 +0900

    통계 페이지 작성 중

--- front-end/statistics/chartInstance/barInstance.js
+++ front-end/statistics/chartInstance/barInstance.js
@@ -1,6 +1,6 @@
-import ChartBase from "../../functional/Chart/Charts";
+import Chart from 'chart.js'
 
-const canvas = document.querySelector('#func1-chart-bar');
+const ctx = document.querySelector('#func1-chart-bar').getContext('2d');
 const dataBox = {
     labels: [1, 2, 3, 4, 5, 7],
     datasets: [
@@ -11,8 +11,26 @@ const dataBox = {
         }
     ]
 };
-const option = {
-}
 
-const barInstance = new ChartBase('bar', canvas, dataBox, option);
+const barInstance = new Chart(ctx, {
+    type: 'bar',
+    data: dataBox,
+    options: {
+
+        onAnimationProgress: function () {
+            const sourceCanvas = this.chart.ctx.canvas;
+            const copyWidth = this.scales["x-axis-0"].paddingLeft - 5;
+            const copyHeight = this.scales["y-axis-0"].bottom + 5;
+            console.log(this);
+            const targetCtx = document.getElementById("func1-chart-bar-axis").getContext("2d");
+
+            targetCtx.canvas.width = copyWidth;
+
+            targetCtx.drawImage(sourceCanvas, 0, 0, copyWidth, copyHeight, 0, 0, copyWidth, copyHeight);
+        }
+
+    }
+});
+
+
 export default barInstance;
--- front-end/statistics/execute.js
+++ front-end/statistics/execute.js
@@ -2,10 +2,11 @@ import barInstance from "./chartInstance/barInstance"
 import lineInstance from "./chartInstance/lineInstance"
 
 function execute() {
-    barInstance.create();
+
     lineInstance.create();
     barInstance.update();
     lineInstance.update();
 }
 
 execute();
+
