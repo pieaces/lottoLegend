0b7eac79d8d4abb1ed5f6fcddea51a0173ccea81
pieaces <jaesung446@gmai.com>
2020-02-23 22:14:07 +0900

    image first commit

--- API/Lotto/class/Generator/Base.ts
+++
@@ -1,9 +0,0 @@
-import { GeneratorOption } from "../../interface/Generator";
-
-export default class Base{
-    option: GeneratorOption;
-
-    constructor(option:GeneratorOption) {
-        this.option = option;
-    }
-}
--- API/index.ts
+++ API/index.ts
@@ -1,9 +1,54 @@
-import awsServerlessExpress from 'aws-serverless-express';
-import app from './app';
+import AWS from 'aws-sdk'
+import dataUriToBuffer from 'data-uri-to-buffer';
+import Jimp from 'jimp';
+const s3 = new AWS.S3();
+const headers = {
+    "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+    //"Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
+}
 
-const server = awsServerlessExpress.createServer(app);
+function attachTimestamp(fileName: string): string {
+    const date = new Date();
+    return `${date.toISOString()}-${fileName}`;
+}
+function putImage(buffer: Buffer, fileName: string): Promise<AWS.S3.PutObjectOutput> {
+    var params = {
+        Body: buffer,
+        Bucket: "canvas-lotto",
+        Key: "images/" + fileName
+    };
+    return new Promise((resolve, reject) => {
+        s3.putObject(params, function (err, data) {
+            if (err) reject(err);
+            else {
+                resolve(data);
+            }
+        });
+    });
+}
 
-exports.handler = (event:any, context:any) => {
-  console.log(`EVENT: ${JSON.stringify(event)}`);
-  awsServerlessExpress.proxy(server, event, context);
+exports.handler = async (event: any) => {
+    console.log(event);
+    let body: any;
+
+    const imagePathList: AWS.S3.PutObjectOutput[] = [];
+    const { imageList } = JSON.parse(event.body);
+    imageList.forEach(async (image: { name: string; src: string; }) => {
+        const fileName = attachTimestamp(image.name);
+        const decoded = dataUriToBuffer(image.src);
+
+        const buffer = await Jimp.read(decoded)
+            .then(image => image.resize(Jimp.AUTO, 300))
+            .then(image => image.quality(70))
+            .then(image => image.getBufferAsync(Jimp.AUTO.toString()));
+
+        imagePathList.push(await putImage(buffer, fileName));
+    });
+
+    const response = {
+        statusCode:200,
+        headers,
+        body: JSON.stringify(imagePathList),
+    };
+    return response;
 };
