bfdf7a43d2b8ba9920d627aa051c772edc521dec
sso <sso9524@naver.com>
2020-02-25 18:56:22 +0900

    사이트맵 정리와 번호게시판 작업 중

---
+++ front-end/components/aside/community.js
@@ -0,0 +1,8 @@
+document.write(`<div class="anchor-title">
+커뮤니티
+</div >
+    <ul>
+        <li> <a href="freeBoardList.html">자유게시판</a> </li>
+        <li> <a href="includeNumList.html">예상 포함번호 게시판</a> </li>
+        <li> <a href="excludeNumList.html">예상 제외번호 게시판</a> </li>
+    </ul>`);
