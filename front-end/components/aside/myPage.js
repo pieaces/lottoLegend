3e79a717075d259bbb9498fa446d067b7dc109ec
sso <sso9524@naver.com>
2020-02-26 12:38:45 +0900

    mypage.js 수정

--- front-end/components/aside/myPage.js
+++ front-end/components/aside/myPage.js
@@ -2,7 +2,7 @@ document.write(`<div class="anchor-title">
 마이페이지
 </div>
 <ul>
-<li> <a href="myLottoList.html">나의 번호조합리스트</a> </li>
+<li> <a href="myLottoList.html">번호조합리스트</a> </li>
 <li> <a href="oftenAsk.html">결제내역</a> </li>
 <li> <a href="qA.html">회원탈퇴</a> </li>
 </ul>`);
