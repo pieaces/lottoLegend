74b946eebb5cf837ced8c3baaaba980c6752264d
pieaces <jaesung446@gmai.com>
2020-02-26 11:40:53 +0900

    comments 파일 옮김

--- front-end/src/qARead.ts
+++ front-end/src/qARead.ts
@@ -8,6 +8,9 @@ const created = document.getElementById('author-time');
 const hits = document.getElementById('author-lookup');
 const contents = document.getElementById('text-content');
 
+const commentContainerBox = document.querySelector('.comment-container-box');
+const commentNum = document.querySelector('#comment-num');
+
 const id = getQueryStringObject().id;
 getUnAuthAPI('/posts/' + id)
     .then(result => {
@@ -18,7 +21,9 @@ getUnAuthAPI('/posts/' + id)
         created.textContent = post.created;
         hits.textContent = post.hits;
         contents.innerHTML = post.contents.text;
-        console.log(post);
+        if(post.comments){
+            
+        }
     });
 
 function getQueryStringObject(): any {
@@ -34,3 +39,53 @@ function getQueryStringObject(): any {
     }
     return result;
 }
+
+function InputCommentData(objArr) {
+    commentNum.textContent = objArr.length;
+    for (let i = 0; i < objArr.length; i++) {
+        const commentBox = document.createElement('div');
+        commentBox.classList.add('commentBox');
+        commentBox.setAttribute('data-id', objArr[i].id);
+
+        const commentTitle = document.createElement('div');
+        commentTitle.classList.add('comment-title');
+
+        const commentAuthor = document.createElement('div');
+        commentAuthor.classList.add('comment-author');
+        commentAuthor.textContent = objArr[i].writerName;
+
+        const commentTime = document.createElement('div');
+        commentTime.classList.add('comment-time');
+        commentTime.textContent = objArr[i].created;
+
+        commentTitle.appendChild(commentAuthor);
+        commentTitle.appendChild(commentTime);
+
+        const commentContent = document.createElement('div');
+        commentContent.classList.add('comment-content');
+        commentContent.textContent = objArr[i].contents;
+
+        commentBox.appendChild(commentTitle);
+        commentBox.appendChild(commentContent);
+
+        commentContainerBox.appendChild(commentBox);
+    }
+}
+const txtArea = document.querySelector('#comment-write-text');
+txtArea.addEventListener('input', limitTxtAreaCount(txtArea))
+function limitTxtAreaCount(target) {
+    const maxlength = 150;
+    const charCurrentCount = document.querySelector('#char-current-count');
+
+    return function () {
+        const currentLength = (target.value.replace(/\s/gi, '')).length;
+
+        if (currentLength > maxlength) {
+            target.value = target.value.slice(0, target.value.length - 1);
+            return false;
+        } else {
+            charCurrentCount.textContent = currentLength;
+            return true;
+        }
+    }
+};
