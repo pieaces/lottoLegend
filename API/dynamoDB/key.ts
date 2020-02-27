e7394e1ecde0fa50173bab06493bc8cbb99539b1
pieaces <jaesung446@gmai.com>
2020-02-27 20:19:31 +0900

    key 삭제

--- API/dynamoDB/key.ts
+++
@@ -1,7 +0,0 @@
-const key = {
-    "accessKeyId": "AKIAWUSQ4J22RHMAVKXB",
-    "secretAccessKey": "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",
-    "region": "ap-northeast-2"
-};
-
-export default key;
