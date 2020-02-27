10cfe8e6725d4de9ff4a6d00f9f435d35fcfa177
pieaces <jaesung446@gmai.com>
2020-02-28 01:31:31 +0900

    getRank 생성

---
+++ API/dynamoDB/getRank.ts
@@ -0,0 +1,41 @@
+export function getRank(userName:string):Promise<string>{
+    const params = {
+        TableName:'lottoUsers',
+        ExpressionAttributeNames,
+        ProjectionExpression: '#Plan, #Until',
+        Key: {
+            "UserName": {
+                S: userName
+            }
+        }
+    };
+    return new Promise((resolve, reject) => {
+        dynamoDB.getItem(params, (err, data) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                const item = data.Item;
+                let rank = Plan.default;
+                if ('Plan' in item && 'Until' in item) {
+                    const plan = item.Plan.S;
+                    const now = Number(new Date());
+                    const until = Number(new Date(item.Until.S));
+                    if (now <= until) {
+                        switch (plan) {
+                            case Plan.basic:
+                                rank = Plan.basic;
+                                //availability = planValues[Plan.basic];
+                                break;
+                            case Plan.premium:
+                                rank = Plan.premium;
+                                //availability = planValues[Plan.premium];
+                                break;
+                        }
+                    }
+                }
+                resolve(rank);
+            }
+        });
+    });
+}
