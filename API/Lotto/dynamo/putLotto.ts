19b81c38a1be63e18e5f4bce1a10f10fe3e949eb
pieaces <jaesung446@gmai.com>
2020-02-28 22:43:56 +0900

    각개 통계 입힘

--- API/Lotto/dynamo/putLotto.ts
+++ API/Lotto/dynamo/putLotto.ts
@@ -1,16 +1,36 @@
 import axios from 'axios'
 import { LData } from '../interface/Lotto';
 import dynamoDB from '.'
+import { Method } from '../interface/LottoDB';
+import Calculate from '../class/Calculate';
+
+function lottoDataParser(data:any):LData{
+    const numbers = [data.drwtNo1, data.drwtNo2, data.drwtNo3, data.drwtNo4, data.drwtNo5, data.drwtNo6]
+    const lotto: LData = { round: data.drwNo, numbers, date: data.drwNoDate, bonusNum: data.bnusNo };
+    return lotto;
+}
+function carryCounter(data:[number[], number[]]): number{
+    const before = data[0];
+    const after = data[1];
+    let count = 0;
+    before.forEach(num =>{
+        if(after.indexOf(num) !== -1) count++;
+    });
+    return count;
+}
 
 export default async function putLotto(round: number):Promise<void> {
     const result = await (axios.get('http://www.nlotto.co.kr/common.do?method=getLottoNumber&drwNo=' + round));
+    const lotto: LData = lottoDataParser(result.data);    
+    let before:any = null;
+    let beforeLotto: LData = null;
+    if(round>1) {
+        before = await (axios.get('http://www.nlotto.co.kr/common.do?method=getLottoNumber&drwNo=' + (round-1)));
+        beforeLotto = lottoDataParser(before.data);
+    }
     console.log(`${round}회 읽기 성공`);
 
-    const data = result.data;
-    const numbers = [data.drwtNo1, data.drwtNo2, data.drwtNo3, data.drwtNo4, data.drwtNo5, data.drwtNo6]
-    const lotto: LData = { round: data.drwNo, numbers, date: data.drwNoDate, bonusNum: data.bnusNo };
-
-    var params = {
+    var params:any = {
         Item: {
             "Round": {
                 N: lotto.round.toString()
@@ -23,25 +43,62 @@ export default async function putLotto(round: number):Promise<void> {
             },
             "BonusNum": {
                 N: lotto.bonusNum.toString()
-            }
+            },
+            [Method.excludedLineCount]:{
+                N: Calculate.excludedLineCount(lotto.numbers).toString()
+            },
+            [Method.lowCount]:{
+                N: Calculate.lowCount(lotto.numbers).toString()
+            },
+            [Method.excludedLineCount]:{
+                N: Calculate.excludedLineCount(lotto.numbers).toString()
+            },
+            [Method.sum]:{
+                N: Calculate.sum(lotto.numbers).toString()
+            },
+            [Method.oddCount]:{
+                N: Calculate.oddCount(lotto.numbers).toString()
+            },
+            [Method.primeCount]:{
+                N: Calculate.primeCount(lotto.numbers).toString()
+            },
+            [Method.$3Count]:{
+                N: Calculate.$3Count(lotto.numbers).toString()
+            },
+            [Method.sum$10]:{
+                N: Calculate.sum$10(lotto.numbers).toString()
+            },
+            [Method.diffMaxMin]:{
+                N: Calculate.diffMaxMin(lotto.numbers).toString()
+            },
+            [Method.AC]:{
+                N: Calculate.AC(lotto.numbers).toString()
+            },
+            [Method.consecutiveExist]:{
+                N: Calculate.consecutiveExist(lotto.numbers).toString()
+            },
         },
         TableName: "LottoData"
     };
-
+    if (round > 1) {
+        params.Item[Method.carryCount] = {
+            N: carryCounter([beforeLotto.numbers, lotto.numbers]).toString()
+        };
+    }
     return new Promise((resolve, reject) => {
-        dynamoDB.putItem(params, function (err, data) {
+        dynamoDB.putItem(params, function (err:any) {
             if (err) reject('putLotto 에러: ' + err);
             else {
-                console.log('putLotto 성공');
+                console.log(`${round}회 putLotto 성공`);
                 resolve();
             }
         });
     });
 }
 
-export async function writeAllLotto(round: number) {
-    for (let i = 1; i <= round; i++) {
-        const bool = await putLotto(i);
-        console.log(i, bool);
+export async function writeAllLotto(until: number, from:number=1) {
+    for (let i = from; i <= until; i++) {
+        await putLotto(i);
+        console.log('*******')
     }
 }
