28749412a0223ba528b5a2ac373ede1f043bd906
pieaces <jaesung446@gmai.com>
2020-02-29 16:32:20 +0900

    분석시스템 재처리

--- front-end/html/components/aside/anal.js
+++ front-end/html/components/aside/anal.js
@@ -2,8 +2,9 @@ document.write(`<div class="anchor-title">
 분석시스템
 </div >
     <ul>
-        <li> <a href="../analSystem/analIntroduce.html">소개</a> </li>
-        <li> <a href="../analSystem/analRecExc.html">추천/제외수</a> </li>
-        <li> <a href="../analSystem/analPre.html">프리미엄필터</a> </li>
-        <li> <a href="../analSystem/analPreExp.html">프리미엄필터 체험</a> </li>
+        <li> <a href="../analSystem/analPreExp.html">체험</a> </li>
+        <li> <a href="../analSystem/analFreeGen.html">무료조합 생성기</a> </li>
+        <li> <a href="../analSystem/?.html">포함수 생성기</a> </li>
+        <li> <a href="../analSystem/?.html">제외수 생성기</a> </li>
+        <li> <a href="../analSystem/analPre.html">프리미엄 필터</a> </li>
     </ul>`);
