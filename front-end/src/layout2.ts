fa477994adfd4b4c07a617cd7e7b3e0c6032a0b1
sso <sso9524@naver.com>
2020-03-01 18:07:15 +0900

    2번 레이아웃 수정, swtich 작성

--- front-end/src/layout2.ts
+++ front-end/src/layout2.ts
@@ -1,17 +1,17 @@
 import configure from './amplify/configure'
-import {getUnAuthAPI} from './amplify/api'
+import { getUnAuthAPI } from './amplify/api'
 import Layout2 from "./functional/Layout/Layout2";
 import Question from './functional/Question';
 
 configure();
 getUnAuthAPI('/stats/mass/excludeInclude')
-.then(data => {
-    console.log(data);
-    const question = new Question();
-    question.numBoardQue.on();
-    const layout = new Layout2([null,null,null,true], data.data, data.winNums, data.total);
-    layout.init();
-    layout.includeVerson();
-    layout.setOpacity();
-    layout.refreshNumberBoard();
-});
+    .then(data => {
+        console.log(data);
+        const question = new Question();
+        question.numBoardQue.on();
+        const layout = new Layout2([null, null, null, true], data.data, data.winNums, data.total);
+        layout.init();
+        layout.includeVerson();
+        layout.setOpacity();
+        layout.refreshNumberBoard();
+    });
