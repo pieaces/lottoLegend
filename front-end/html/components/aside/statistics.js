defe1b0b412e6c477da90e32063bc21c982d85fe
pieaces <jaesung446@gmai.com>
2020-03-02 19:32:30 +0900

    merge

--- front-end/html/components/aside/statistics.js
+++ front-end/html/components/aside/statistics.js
@@ -4,15 +4,15 @@ document.write(`
 <li> <a href="../incExcBoard/incExcNumList.html">포함/제외수 공유</a> </li>
 <li class="arrow-box"> <a href="../statistics/statistics.html">통계자료</a><i
         class="fas fa-sort-up filter-arrow"></i> </li>
-<li> <a href="#">통계 홈</a> </li>
-<li> <a href="?method=excludedLineCount">전멸구간 개수</a> </li>
-<li> <a href="?method=carryCount">이월 개수</a> </li>
-<li> <a href="?method=lowCount">저값(1~22) 개수</a> </li>
-<li> <a href="?method=sum">번호 합계</a> </li>
-<li> <a href="?method=oddCount">홀수 개수</a> </li>
-<li> <a href="?method=primeCount">소수 개수</a> </li>
-<li> <a href="?method=$3Count">3배수 개수</a> </li>
-<li> <a href="?method=sum$10">첫수(십의자리) 합</a> </li>
-<li> <a href="?method=diffMaxMin">고저차</a> </li>
-<li> <a href="?method=AC">AC</a> </li>
+<li> <a href="statHome.html">통계 홈</a> </li>
+<li> <a href="statistics.html?method=excludedLineCount">전멸구간 개수</a> </li>
+<li> <a href="statistics.html?method=carryCount">이월 개수</a> </li>
+<li> <a href="statistics.html?method=lowCount">저값(1~22) 개수</a> </li>
+<li> <a href="statistics.html?method=sum">번호 합계</a> </li>
+<li> <a href="statistics.html?method=oddCount">홀수 개수</a> </li>
+<li> <a href="statistics.html?method=primeCount">소수 개수</a> </li>
+<li> <a href="statistics.html?method=$3Count">3배수 개수</a> </li>
+<li> <a href="statistics.html?method=sum$10">첫수(십의자리) 합</a> </li>
+<li> <a href="statistics.html?method=diffMaxMin">고저차</a> </li>
+<li> <a href="statistics.html?method=AC">AC</a> </li>
 </ul>`)
