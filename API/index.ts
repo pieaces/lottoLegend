91947f6a16af79015dd77061889991386c769bca
pieaces <jaesung446@gmai.com>
2020-03-03 19:11:56 +0900

    posts

--- API/index.ts
+++ API/index.ts
@@ -1,14 +1,9 @@
-import { LottoNumber } from "./interface/Lotto";
-import { StatsMethod, QueryStatsParams } from "./interface/LottoDB";
 import verify from "./auth";
-import { Response } from "./class";
-import {getCurrentRound, isIdentical} from "./funtions";
+import { getCurrentRound, isIdentical } from "./funtions";
 
 import Posts from "./mariaDB/Posts";
 import Comments from "./mariaDB/Comments";
-import { getPlan, Plan } from "./dynamoDB/userInfo";
-import { queryStats, queryLotto } from "./dynamoDB/lottoData";
-import { updateNumbers, getNumbersByClass, deleteNumsArr, getIncOrExcNumbers, getNumbersByRound, updateIncOrExcNumbers, IncOrExc } from './dynamoDB/myNumbers'
+import { getIncOrExcNumbers, IncOrExc } from './dynamoDB/myNumbers'
 
 const headers = {
     "Access-Control-Allow-Origin": "*", // Required for CORS support to work
@@ -41,64 +36,6 @@ exports.handler = async (event: any) => {
     let body: any;
 
     switch (resource) {
-        case '/stats/mass/{method}': {
-            const method = event.pathParameters.method;
-            let data;
-            if (method === "excludeInclude") {
-                let temp: any = {};
-                temp.emergence = await queryStats("emergence" as StatsMethod, {});
-                temp.interval = await queryStats("interval" as StatsMethod, {});
-                temp.howLongNone = await queryStats("howLongNone" as StatsMethod, {});
-                temp.frequency = await queryStats("frequency" as StatsMethod, {});
-
-                data = temp;
-                let round = getCurrentRound();
-                let total: number = 0;
-
-                const winNums: LottoNumber[][] = [];
-                while (winNums.length !== 3) {
-                    try {
-                        const numbers = await queryLotto(round);
-                        winNums.push(numbers);
-                        if (total === 0) total = round;
-                    } catch (err) {
-                        console.log(err);
-                    } finally {
-                        round--;
-                    }
-                }
-                body = { data, total, winNums };
-            } else if (method in StatsMethod) {
-                const plan = await getPlan(currentId);
-                if (plan !== Plan.premium && plan !== Plan.premiumplus) {
-                    return {
-                        statusCode: 200,
-                        headers,
-                        body: JSON.stringify(new Response(true, 'Not Premium'))
-                    }
-                }
-                const temp: QueryStatsParams = {}
-                const from = event.queryStringParameters && event.queryStringParameters.from
-                const to = event.queryStringParameters && event.queryStringParameters.to;
-                const list = event.queryStringParameters && event.queryStringParameters.list
-                if ((from && to)) {
-                    temp.from = Number(from);
-                    temp.to = Number(to);
-                } else if (list) {
-                    temp.list = JSON.parse(decodeURI(list));
-                }
-                data = await queryStats(method as StatsMethod, temp);
-                body = { data };
-            } else {
-                body = 'wrong method';
-            }
-        }
-            break;
-        case '/stats/piece/{method}': {
-            const method = event.pathParameters.method;
-            body = await queryStats(method as StatsMethod, null, 'Ideal.#All, Actual.#All, Pos, Stats, Piece', { "#All": 'all' });
-        }
-            break;
         case '/posts': {
             const db = new Posts();
             switch (method) {
@@ -212,90 +149,6 @@ exports.handler = async (event: any) => {
             }
         }
             break;
-        /*
-        rank = default, basic, premium
-        method = auto, manual, include, exclude
-        numbers/mass|piece/
-        */
-        case '/numbers/mass': {
-            if (logedIn) {
-                switch (method) {
-                    case 'GET': {
-                        const round = event.queryStringParameters.round;
-                        const tool = event.queryStringParameters.tool;
-                        const selectMethod = event.queryStringParameters.method;
-                        if (!tool) {
-                            body = await getNumbersByRound(currentId, round);
-                        } else {
-                            if (selectMethod) {
-                                const { numsArr } = await getNumbersByClass(currentId, round, { tool, method: selectMethod });
-                                body = numsArr;
-                            } else {
-                                body = await getNumbersByClass(currentId, round, { tool, method: selectMethod });
-                            }
-                        }
-                    }
-                        break;
-                    case 'POST':
-                        const { numsArr, tool } = JSON.parse(event.body)
-                        body = await updateNumbers(currentId, getCurrentRound(), numsArr, tool);
-                        break;
-                }
-            } else {
-                statusCode = 400;
-                body = "로그인되지 않은 사용자입니다."
-            }
-        }
-            break;
-        case '/numbers/mass/{index}': {
-            const index = event.pathParameters.index;
-            if (logedIn) {
-                switch (method) {
-                    case 'DELETE':
-                        const { selectMethod, tool } = JSON.parse(event.body)
-                        await deleteNumsArr(currentId, getCurrentRound(), { tool, method: selectMethod }, index);
-                        break;
-                }
-            } else {
-                statusCode = 400;
-                body = "로그인되지 않은 사용자입니다."
-            }
-        }
-            break;
-        case '/numbers/piece': {
-            switch (method) {
-                case 'GET': {
-                    const userName = event.queryStringParameters && event.queryStringParameters.userName || currentId;
-                    const round = event.queryStringParameters && event.queryStringParameters.round || getCurrentRound();
-                    const choice = event.queryStringParameters && event.queryStringParameters.choice;
-                    if (choice) {
-                        const numbers = await getIncOrExcNumbers(userName, round, choice);
-                        body = numbers;
-                    } else {
-                        const include = await getIncOrExcNumbers(userName, round, IncOrExc.include);
-                        const exclude = await getIncOrExcNumbers(userName, round, IncOrExc.exclude);
-                        body = { include, exclude };
-                    }
-                }
-                    break;
-                case 'POST':
-                    if (logedIn) {
-                        const { numbers, choice } = JSON.parse(event.body);
-                        await updateIncOrExcNumbers(currentId, getCurrentRound(), numbers.sort((a:number,b:number) => a-b), choice);
-                        break;
-                    } else {
-                        statusCode = 400;
-                        body = "로그인되지 않은 사용자입니다."
-                    }
-                    break;
-                /*
-            case 'DELETE':
-                await deleteNumbers(userName, round, choice);
-                break;
-                */
-            }
-            break;
-        }
     }
     const response = {
         statusCode,
