bb9e2c469e4ec5b7b846932d94148805dfce0947
pieaces <jaesung446@gmai.com>
2020-03-03 15:18:16 +0900

    포함수,제외수 생성기 분리

--- front-end/html/components/aside/analSystem.js
+++ front-end/html/components/aside/analSystem.js
@@ -2,8 +2,10 @@ document.write(`<div class="anchor-title">
 분석 시스템
 </div >
     <ul>
-        <li> <a href="../analSystem/analPreExp.html">체험</a> </li>
-        <li> <a href="../analSystem/analIncExcGen.html">포함/제외수 생성기</a> </li>
+        <li> <a href="../analSystem/analPreExp.html">체험하기</a> </li>
+        <li> 생성기 </li>
+        <li> <a href="../analSystem/analIncGen.html">추천수</a> </li>
+        <li> <a href="../analSystem/analExcGen.html">제외수</a> </li>
         <li> <a href="../analSystem/analFreeGen.html">무료조합 생성기</a> </li>
         <li> <a href="../analSystem/analPre.html">프리미엄 필터</a> </li>
     </ul>`);
--- front-end/src/analIncExcGen.ts
+++ front-end/src/analIncExcGen.ts
@@ -6,14 +6,15 @@ import ResetBtn from './functional/instanceBtns/ResetBtn';
 import Swal from 'sweetalert2'
 import { infoAlert, networkAlert } from './functions';
 
-const makeBtn = document.getElementById('make-btn');
+configure();
 
+const category = document.querySelector('.main').getAttribute('data-category');
+const makeBtn = document.getElementById('make-btn');
 enum IncOrExc {
     "include" = "Include",
     "exclude" = "Exclude"
 }
-let incOrexc: IncOrExc = IncOrExc.include;
-configure();
+let incOrExc: IncOrExc;
 const loading = document.querySelector<HTMLElement>('.loading');
 loading.classList.remove('none');
 getUnAuthAPI('/stats/mass/excludeInclude')
@@ -25,10 +26,22 @@ getUnAuthAPI('/stats/mass/excludeInclude')
             const layout = new Layout2([null, null, null, true], data.data, data.winNums, data.total);
             loading.classList.add('none');
             layout.init();
-            layout.includeVerson();
+            let alertMessage: string[] = [];
+            let numbers:number[];
+            if (category === "include") {
+                layout.includeVerson();
+                numbers = exclude;
+                incOrExc = IncOrExc.include;
+                alertMessage = ['이번회차 제외수가 저장 되어있습니다', '기존에 생성하셨던 제외수를 추천수 생성에 반영할까요?'];
+            } else {
+                layout.excludeVersion();
+                numbers = include;
+                incOrExc = IncOrExc.exclude;
+                alertMessage = ['이번회차 추천수가 저장 되어있습니다', '기존에 생성하셨던 추천수를 제외수 생성에 반영할까요?'];
+            }
             layout.setOpacity();
-            if (exclude.length > 0 && (await infoAlert('이번회차 제외수가 저장 되어있습니다', '기존에 생성하셨던 제외수를 포함수 생성에 반영할까요?')).value) {
-                layout.options[5] = exclude;
+            if (numbers.length > 0 && (await infoAlert(alertMessage[0], alertMessage[1])).value) {
+                layout.options[5] = numbers;
                 layout.setOpacity();
             }
             layout.refreshNumberBoard();
@@ -37,22 +50,44 @@ getUnAuthAPI('/stats/mass/excludeInclude')
 
             makeBtn.addEventListener('click', async () => {
                 if (include.length === 0 || layout.checkedNumbers.length === 0 && (await infoAlert('선택하신 번호가 없습니다', '빈 상태로 초기화합니다')).value ||
-                    (include.length > 0 && layout.checkedNumbers.length > 0) && (await infoAlert('이미 번호를 생성하셨습니다', '초기화하고 덮어씁니다')).value) {
+                    (include.length > 0 && layout.checkedNumbers.length > 0) && (await infoAlert('기존에 번호를 생성하셨습니다', '초기화하고 덮어씁니다')).value) {
                     try {
-                        await postAuthAPI('/numbers/piece', { numbers: layout.checkedNumbers, choice: incOrexc });
-                        Swal.fire({
-                            title: '완료!',
-                            text: '이어서 제외수를 생성하시겠습니까?',
-                            icon: 'success',
-                            showCancelButton: true,
-                            confirmButtonColor: '#3085d6',
-                            cancelButtonColor: '#d33',
-                            confirmButtonText: '네',
-                            cancelButtonText: '아니요',
-                        }).then(async (result) => {
-                            if (result.value) {
-                            }
-                        });
+                        await postAuthAPI('/numbers/piece', { numbers: layout.checkedNumbers, choice: incOrExc });
+                        if (category === "include") {
+                            Swal.fire({
+                                title: '완료!',
+                                text: '이어서 제외수를 생성하시겠습니까?',
+                                icon: 'success',
+                                showCancelButton: true,
+                                confirmButtonColor: '#3085d6',
+                                cancelButtonColor: '#d33',
+                                confirmButtonText: '네',
+                                cancelButtonText: '아니요',
+                            }).then(async (result) => {
+                                if (result.value) {
+                                    location.href = `./analExcGen.html`;
+                                } else {
+
+                                }
+                            });
+                        } else {
+                            Swal.fire({
+                                title: '완료!',
+                                text: '이어서 추천수를 생성하시겠습니까?',
+                                icon: 'success',
+                                showCancelButton: true,
+                                confirmButtonColor: '#3085d6',
+                                cancelButtonColor: '#d33',
+                                confirmButtonText: '네',
+                                cancelButtonText: '아니요',
+                            }).then(async (result) => {
+                                if (result.value) {
+                                    location.href = `./analIncGen.html`;
+                                } else {
+
+                                }
+                            });
+                        }
                     } catch (err) {
                         networkAlert();
                     }
