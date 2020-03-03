ba7fba21241f24332a0684440d34b14037ec23ee
pieaces <jaesung446@gmai.com>
2020-03-03 19:30:24 +0900

    stats-mass

--- API/index.ts
+++ API/index.ts
@@ -1,8 +1,7 @@
 import { LottoNumber } from "./interface/Lotto";
 import { StatsMethod, QueryStatsParams } from "./interface/LottoDB";
 import verify from "./auth";
-import { Response } from "./class";
-import {getCurrentRound, isIdentical} from "./funtions";
+import { getCurrentRound } from "./funtions";
 
 import { getPlan, Plan } from "./dynamoDB/userInfo";
 import { queryStats, queryLotto } from "./dynamoDB/lottoData";
@@ -13,17 +12,11 @@ const headers = {
 }
 exports.handler = async (event: any) => {
     console.log(event);
-    const method: string = event.httpMethod;
-    const resource: string = event.resource;
-    let logedIn: boolean = false;
-
-    let currentId: string, currentName: string;
+    let currentId: string;
     if (event.headers['x-id-token']) {
         try {
             const userInfo = verify(event.headers['x-id-token']);
-            logedIn = true;
             currentId = userInfo["cognito:username"];
-            currentName = userInfo.nickname;
         } catch (err) {
             console.log('Intruder Alert! - Expired Token', err);
             const response = {
@@ -36,61 +29,55 @@ exports.handler = async (event: any) => {
 
     let statusCode: number = 200;
     let body: any;
+    const method = event.pathParameters.method;
+    let data:any;
+    if (method === "excludeInclude") {
+        let temp: any = {};
+        temp.emergence = await queryStats("emergence" as StatsMethod, {});
+        temp.interval = await queryStats("interval" as StatsMethod, {});
+        temp.howLongNone = await queryStats("howLongNone" as StatsMethod, {});
+        temp.frequency = await queryStats("frequency" as StatsMethod, {});
 
-    switch (resource) {
-        case '/stats/mass/{method}': {
-            const method = event.pathParameters.method;
-            let data;
-            if (method === "excludeInclude") {
-                let temp: any = {};
-                temp.emergence = await queryStats("emergence" as StatsMethod, {});
-                temp.interval = await queryStats("interval" as StatsMethod, {});
-                temp.howLongNone = await queryStats("howLongNone" as StatsMethod, {});
-                temp.frequency = await queryStats("frequency" as StatsMethod, {});
+        data = temp;
+        let round = getCurrentRound();
+        let total: number = 0;
 
-                data = temp;
-                let round = getCurrentRound();
-                let total: number = 0;
-
-                const winNums: LottoNumber[][] = [];
-                while (winNums.length !== 3) {
-                    try {
-                        const numbers = await queryLotto(round);
-                        winNums.push(numbers);
-                        if (total === 0) total = round;
-                    } catch (err) {
-                        console.log(err);
-                    } finally {
-                        round--;
-                    }
-                }
-                body = { data, total, winNums };
-            } else if (method in StatsMethod) {
-                const plan = await getPlan(currentId);
-                if (plan !== Plan.premium && plan !== Plan.premiumplus) {
-                    return {
-                        statusCode: 200,
-                        headers,
-                        body: JSON.stringify(new Response(true, 'Not Premium'))
-                    }
-                }
-                const temp: QueryStatsParams = {}
-                const from = event.queryStringParameters && event.queryStringParameters.from
-                const to = event.queryStringParameters && event.queryStringParameters.to;
-                const list = event.queryStringParameters && event.queryStringParameters.list
-                if ((from && to)) {
-                    temp.from = Number(from);
-                    temp.to = Number(to);
-                } else if (list) {
-                    temp.list = JSON.parse(decodeURI(list));
-                }
-                data = await queryStats(method as StatsMethod, temp);
-                body = { data };
-            } else {
-                body = 'wrong method';
+        const winNums: LottoNumber[][] = [];
+        while (winNums.length !== 3) {
+            try {
+                const numbers = await queryLotto(round);
+                winNums.push(numbers);
+                if (total === 0) total = round;
+            } catch (err) {
+                console.log(err);
+            } finally {
+                round--;
             }
         }
-            break;
+        body = { data, total, winNums };
+    } else if (method in StatsMethod) {
+        const plan = await getPlan(currentId);
+        if (plan !== Plan.premium && plan !== Plan.premiumplus) {
+            console.log('Intruder Alert - Not Premium');
+            return {
+                statusCode: 400,
+                headers
+            }
+        }
+        const temp: QueryStatsParams = {}
+        const from = event.queryStringParameters && event.queryStringParameters.from
+        const to = event.queryStringParameters && event.queryStringParameters.to;
+        const list = event.queryStringParameters && event.queryStringParameters.list
+        if ((from && to)) {
+            temp.from = Number(from);
+            temp.to = Number(to);
+        } else if (list) {
+            temp.list = JSON.parse(decodeURI(list));
+        }
+        data = await queryStats(method as StatsMethod, temp);
+        body = { data };
+    } else {
+        body = 'wrong method';
     }
     const response = {
         statusCode,
