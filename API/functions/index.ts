82e1a6cdde013fc49ac0f644fbaebe75a390306d
pieaces <jaesung446@gmai.com>
2020-03-04 18:58:18 +0900

    generator 분리 독립

--- API/functions/index.ts
+++
@@ -1,7 +0,0 @@
-export function getCurrentRound(currentDate?: string): number {
-    const theDate = new Date('2020-02-01:11:45');
-    const today = currentDate && new Date(currentDate) || new Date();
-    const between = Number(today) - Number(theDate);
-    const plusDate = Math.floor(between / 24 / 3600 / 1000 / 7);
-    return 896 + plusDate;
-}
--- API/index.ts
+++ API/index.ts
@@ -20,11 +20,10 @@ const headers = {
 
 exports.handler = async (event: any) => {
     console.log(event);
-    const resource = event.resource;
 
     let currentId: string;
     try {
-        const userInfo = jwt.verify(event.headers['x-id-token'], pem, { algorithms: ['RS256'] });
+        const userInfo:any = jwt.verify(event.headers['x-id-token'], pem, { algorithms: ['RS256'] });
         currentId = userInfo["cognito:username"];
     } catch (err) {
         console.log('Intruder Alert! - Expired Token || Not LogedIn', err);
@@ -37,89 +36,83 @@ exports.handler = async (event: any) => {
     let statusCode: number = 200;
     let body: any;
 
-    switch (resource) {
-        case '/generator/premium':
-            const plan = await getRank(currentId);
-            if (plan !== Plan.premium && plan !== Plan.premiumplus) {
-                console.log('Intruder Alert! - Not Premium!');
-                const response = {
-                    statusCode: 400,
-                    headers,
-                };
-                return response;
-            }
-            const option: GeneratorOption = {};
-            let willRangeFinder: (numbers: number[]) => number = null;
-            const { options } = JSON.parse(event.body);
-
-            if (options.excludedLines) {
-                option.excludedLines = options.excludedLines;
-            }
-            if (options.includedNumbers) {
-                option.includedNumbers = options.includedNumbers;
-            }
-            if (options.excludedNumbers) {
-                option.excludedNumbers = options.excludedNumbers;
-            }
-            if (options.lowCount) {
-                option.lowCount = options.lowCount;
-            }
-            if (options.sum) {
-                option.sum = options.sum;
-                willRangeFinder = Calculate.oddCount;
-            }
-            if (options.oddCount) {
-                option.oddCount = options.oddCount;
-                willRangeFinder = Calculate.primeCount;
-            }
-            if (options.primeCount) {
-                option.primeCount = options.primeCount;
-                willRangeFinder = Calculate.$3Count;
-            }
-            if (options.$3Count) {
-                option.$3Count = options.$3Count;
-                willRangeFinder = Calculate.sum$10;
-            }
-            if (options.sum$10) {
-                option.sum$10 = options.sum$10;
-                willRangeFinder = Calculate.diffMaxMin;
-            }
-            if (options.diffMaxMin) {
-                option.diffMaxMin = options.diffMaxMin;
-                willRangeFinder = Calculate.AC;
-            }
-            if (options.AC) {
-                option.AC = options.AC;
-                willRangeFinder = Calculate.consecutiveExist;
-            }
-            if (typeof options.consecutiveExist === 'boolean') {
-                option.consecutiveExist = options.consecutiveExist;
-            }
-
-            const generator = new Generator(option);
-            generator.rangeFinder = willRangeFinder;
-            generator.generate();
-
-            if (!generator.option.sum) {
-                statusCode = 400;
-            }
-            else if (generator.option.sum && generator.count <= 50) {
-                body = {
-                    range: [...generator.rangeSet].sort((a, b) => (a - b)),
-                    count: generator.getGeneratedNumbers().length,
-                    numbers: generator.getGeneratedNumbers()
-                };
-            } else {
-                body = {
-                    range: [...generator.rangeSet].sort((a, b) => (a - b)),
-                    count: generator.getGeneratedNumbers().length
-                };
-            }
-            break;
-        case '/generator/free':
-            
-            break;
+    const plan = await getRank(currentId);
+    if (plan !== Plan.premium && plan !== Plan.premiumplus) {
+        console.log('Intruder Alert! - Not Premium!');
+        const response = {
+            statusCode: 400,
+            headers,
+        };
+        return response;
     }
+    const option: GeneratorOption = {};
+    let willRangeFinder: (numbers: number[]) => number = null;
+    const { options } = JSON.parse(event.body);
+
+    if (options.excludedLines) {
+        option.excludedLines = options.excludedLines;
+    }
+    if (options.includedNumbers) {
+        option.includedNumbers = options.includedNumbers;
+    }
+    if (options.excludedNumbers) {
+        option.excludedNumbers = options.excludedNumbers;
+    }
+    if (options.lowCount) {
+        option.lowCount = options.lowCount;
+    }
+    if (options.sum) {
+        option.sum = options.sum;
+        willRangeFinder = Calculate.oddCount;
+    }
+    if (options.oddCount) {
+        option.oddCount = options.oddCount;
+        willRangeFinder = Calculate.primeCount;
+    }
+    if (options.primeCount) {
+        option.primeCount = options.primeCount;
+        willRangeFinder = Calculate.$3Count;
+    }
+    if (options.$3Count) {
+        option.$3Count = options.$3Count;
+        willRangeFinder = Calculate.sum$10;
+    }
+    if (options.sum$10) {
+        option.sum$10 = options.sum$10;
+        willRangeFinder = Calculate.diffMaxMin;
+    }
+    if (options.diffMaxMin) {
+        option.diffMaxMin = options.diffMaxMin;
+        willRangeFinder = Calculate.AC;
+    }
+    if (options.AC) {
+        option.AC = options.AC;
+        willRangeFinder = Calculate.consecutiveExist;
+    }
+    if (typeof options.consecutiveExist === 'boolean') {
+        option.consecutiveExist = options.consecutiveExist;
+    }
+
+    const generator = new Generator(option);
+    generator.rangeFinder = willRangeFinder;
+    generator.generate();
+
+    if (!generator.option.sum) {
+        statusCode = 400;
+    }
+    else if (generator.option.sum && generator.count <= 50) {
+        body = {
+            range: [...generator.rangeSet].sort((a, b) => (a - b)),
+            count: generator.getGeneratedNumbers().length,
+            numbers: generator.getGeneratedNumbers()
+        };
+    } else {
+        body = {
+            range: [...generator.rangeSet].sort((a, b) => (a - b)),
+            count: generator.getGeneratedNumbers().length
+        };
+    }
+
     return {
         statusCode,
         headers,
