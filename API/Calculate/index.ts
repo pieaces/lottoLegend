f9102feca13150d30b4d7333970e771a0674c46f
pieaces <jaesung446@gmai.com>
2020-03-06 00:25:03 +0900

    generator 이전

---
+++ API/Calculate/index.ts
@@ -0,0 +1,98 @@
+//계산: 일차원 데이터 => 영차원 데이터
+export default class Calculate {
+    private constructor() { }
+    static excludedLineCount(numbers: number[]): number {
+        const SIZE = 5;
+        const set = new Set<number>();
+
+        numbers.forEach(num => {
+            set.add(Math.floor(num / 10));
+        });
+
+        return SIZE - set.size;
+    }
+
+    static oddCount(numbers: number[]): number {
+        let count = 0;
+        for(let i =0; i<numbers.length; i++){
+            if(numbers[i] % 2 === 1) count++;
+        }
+        //numbers.forEach(value => {if (value % 2 === 1) count++;});
+        return count;
+    }
+    static primeCount(numbers:number[]): number {
+        let count = 0;
+        numbers.forEach(value => {
+            switch(value){
+                case 2: case 3: case 5: case 7:
+                case 11: case 13: case 17: case 19:
+                case 23: case 29: case 31: case 37:
+                case 41: case 43: 
+                count++;
+            }
+        });
+        return count;
+    }
+    static $3Count(numbers: number[]): number {
+        let count = 0;
+        numbers.forEach(value => {
+            if(value % 3 === 0) count++;
+        });
+        return count;
+    }
+
+    static sum(numbers: number[]): number {
+        let sum=0;
+        for(let i=0; i<numbers.length; i++){
+            sum += numbers[i];
+        }
+        return sum
+        //return numbers.reduce((acc, cur) => acc + cur, 0);
+    }
+    static sum$10(numbers: number[]): number {
+        let sum=0;
+        for(let i=0; i<numbers.length; i++){
+            sum += Math.floor(numbers[i]/10);
+        }
+        return sum
+        //return numbers.reduce((acc, cur) => acc + Math.floor(cur / 10), 0);
+    }
+    static sum$1(numbers: number[]): number {
+        return numbers.reduce((acc, cur) => acc + cur % 10, 0);
+    }
+
+    static min(numbers: number[]): number {
+        return Math.min(...numbers);
+    }
+    static max(numbers: number[]): number {
+        return Math.max(...numbers);
+    }
+
+    static diffMaxMin(numbers: number[]): number {
+        let min=numbers[0], max = numbers[0];
+        for(let i=1; i<numbers.length; i++){
+            if(min > numbers[i]) min = numbers[i];
+            if(max < numbers[i]) max = numbers[i];
+        }
+        return max-min;
+        //return Calculate.max(numbers) - Calculate.min(numbers);
+    }
+
+    static AC(numbers: number[]): number {
+        const set = new Set<number>();
+        for(let i = 0; i<numbers.length; i++){
+            for(let j=i+1; j<numbers.length; j++){
+                set.add(Math.abs(numbers[j]-numbers[i]));
+            }
+        }
+        return set.size - (numbers.length - 1);
+    }
+
+    static lowCount(numbers: number[]): number {
+        let count =0;
+        numbers.forEach(value => {
+            if(value <23) count++;
+        });
+        return count;
+    }
+}
---
+++ API/dynamoDB/generator.ts
@@ -0,0 +1,106 @@
+import Calculate from "../Calculate";
+import { dynamoDB } from ".";
+import { getCurrentRound } from "../funtions";
+import { getIncOrExcNumbers, IncOrExc } from "./myNumbers";
+
+export async function freeGeneratorA(currentId:string) {
+    const currentRound = getCurrentRound();
+    const include = await getIncOrExcNumbers(currentId, currentRound, IncOrExc.include);
+    const exclude = await getIncOrExcNumbers(currentId, currentRound, IncOrExc.exclude);
+    const choice: number[] = [];
+
+    exclude.forEach((num, index) => {
+        for (let i = exclude[index - 1] + 1 || 1; i < num; i++) {
+            choice.push(i);
+        }
+    });
+    for (let i = exclude[exclude.length - 1] + 1; i <= 45; i++) {
+        choice.push(i);
+    }
+
+    const numsArr: number[][] = [];
+
+    while (numsArr.length <= 5) {
+        const SIZE = choice.length;
+        const numberSet: Set<number> = new Set();
+        while (numberSet.size < 6) {
+            numberSet.add(choice[Math.floor(Math.random() * SIZE)]);
+        }
+        const numbers = [...numberSet].sort((a, b) => a - b);
+
+        let flag = false;
+        for (let i = 0; i < 6; i++) {
+            for (let j = 0; j < include.length; j++) {
+                if (numbers[i] === include[j]) {
+                    flag = true;
+                    break;
+                }
+            }
+        }
+        if (flag) {
+            numsArr.push(numbers);
+        }
+    }
+
+    return numsArr;
+}
+async function abc() {
+    const numsArr = await freeGeneratorA('pieaces');
+    numsArr.push([2, 9, 16, 25, 26, 40]);
+    numsArr.push([2, 9, 16, 25, 26, 39]);
+    const lottoData = await bcd();
+    const body = numsArr.map(numbers => {
+        const winner = new Array<number>(5).fill(0);
+
+        lottoData.forEach(lotto => {
+            let count = 0;
+            numbers.forEach(num => {
+                if (lotto.Numbers.NS.indexOf(num.toString()) !== -1) {
+                    count++;
+                }
+            });
+            switch (count) {
+                case 3:
+                    winner[4]++;
+                    break;
+                case 4:
+                    winner[3]++;
+                    break;
+                case 5:
+                    if (numbers.indexOf(Number(lotto.BonusNum.N)) !== -1) {
+                        winner[1]++;
+                    } else {
+                        winner[2]++;
+                    }
+                    break;
+                case 6:
+                    winner[0]++;
+            }
+        });
+        return {
+            numbers,
+            winner,
+            lowCount: Calculate.lowCount(numbers),
+            sum: Calculate.sum(numbers),
+            oddCount: Calculate.oddCount(numbers),
+            primeCount: Calculate.primeCount(numbers),
+            $3Count: Calculate.$3Count(numbers),
+            sum$10: Calculate.sum$10(numbers),
+            diffMaxMin: Calculate.diffMaxMin(numbers),
+            AC: Calculate.AC(numbers)
+        }
+    });
+    return body;
+}
+async function bcd(): Promise<{ BonusNum: { N: string }, Numbers: { NS: string[] } }[]> {
+    const params: AWS.DynamoDB.ScanInput = {
+        TableName: 'LottoData',
+        ProjectionExpression: 'Numbers, BonusNum'
+    };
+    return new Promise((resolve, reject) => {
+        dynamoDB.scan(params, (err, data: any) => {
+            if (err) reject(err);
+            resolve(data.Items);
+        });
+    })
+}
