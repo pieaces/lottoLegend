d09ed95e7b990dee24037384b13ec41ffd358db6
pieaces <jaesung446@gmai.com>
2020-03-07 00:03:28 +0900

    무료생성기 끝

--- front-end/src/freeGenerator.ts
+++ front-end/src/freeGenerator.ts
@@ -105,33 +105,43 @@ async function init() {
     makeLine(includeCanvas, include);
     makeLine(excludeCanvas, exclude);
     makeBtn.addEventListener('click', async () => {
-        let dataSet:any;
-        wrapper.classList.add('none');
-        Swal.fire({
-            title: `<div class="lds-circle"><div></div></div>`,
-            footer:'주어진 데이터로 고객님의 번호를 조합중입니다',
-            allowOutsideClick: false,
-            timer: 1000,
-        }).then(()=>{
-            wrapper.classList.remove('none');
-        });
-        document.querySelector('.swal2-actions').innerHTML = '';
-        const popup = document.querySelector<HTMLElement>('.swal2-popup');
-        popup.style.width = "45rem";
-        popup.style.height = "27.8rem";
-        popup.style.background = 'gold';
-        popup.style.boxShadow = "0 1px 2px rgba(0,0,0,0.07),0 2px 4px rgba(0,0,0,0.07),0 4px 8px rgba(0,0,0,0.07),0 8px 16px rgba(0,0,0,0.07),0 16px 32px rgba(0,0,0,0.07),0 32px 64px rgba(0,0,0,0.07)";
-        const backdrop = document.querySelector<HTMLElement>('.swal2-backdrop-show')
-        backdrop.style.background = "rgba(0, 0, 0, 0.6)";
-        backdrop.style.boxShadow = "rgba(0, 0, 0, 0.7) 19rem 13rem 24rem inset, rgba(0, 0, 0, 0.7) -14rem -9rem 23rem inset";
-        const footer = document.querySelector<HTMLElement>('.swal2-footer');
-        footer.style.margin = '0';
-        footer.style.padding = '1.5em 0 0';
-        footer.style.fontSize = '2rem';
-        footer.style.fontWeight = 'bold';
+        let params: any;
+        if (!lineCheck || lineCheck && sum() === 6) {
+            params = [Number(first.value), Number(tenth.value), Number(twentieth.value), Number(thirtieth.value), Number(fortieth.value)]
+            let dataSet: any;
+            wrapper.classList.add('none');
+            Swal.fire({
+                title: `<div class="lds-circle"><div></div></div>`,
+                footer: '주어진 데이터로 고객님의 번호를 조합중입니다',
+                allowOutsideClick: false,
+                timer: 1000,
+            }).then(() => {
+                wrapper.classList.remove('none');
+            });
+            document.querySelector('.swal2-actions').innerHTML = '';
+            const popup = document.querySelector<HTMLElement>('.swal2-popup');
+            popup.style.width = "45rem";
+            popup.style.height = "27.8rem";
+            popup.style.background = 'gold';
+            popup.style.boxShadow = "0 1px 2px rgba(0,0,0,0.07),0 2px 4px rgba(0,0,0,0.07),0 4px 8px rgba(0,0,0,0.07),0 8px 16px rgba(0,0,0,0.07),0 16px 32px rgba(0,0,0,0.07),0 32px 64px rgba(0,0,0,0.07)";
+            const backdrop = document.querySelector<HTMLElement>('.swal2-backdrop-show')
+            backdrop.style.background = "rgba(0, 0, 0, 0.6)";
+            backdrop.style.boxShadow = "rgba(0, 0, 0, 0.7) 19rem 13rem 24rem inset, rgba(0, 0, 0, 0.7) -14rem -9rem 23rem inset";
+            const footer = document.querySelector<HTMLElement>('.swal2-footer');
+            footer.style.margin = '0';
+            footer.style.padding = '1.5em 0 0';
+            footer.style.fontSize = '2rem';
+            footer.style.fontWeight = 'bold';
 
-        dataSet = await getAuthAPI('/numbers/generator/free');
-        Layout3.makeNumBoard(dataSet);
+            dataSet = await getAuthAPI('/numbers/generator/free', { lineCount: JSON.stringify(params) });
+            Layout3.makeNumBoard(dataSet);
+        }else{
+            Swal.fire({
+                title:'알림',
+                text:'구간개수의 합이 6이 되어야합니다',
+                icon:'info'
+            })
+        }
     });
     loading.classList.add('none');
 }
