e4a762a9cc6827a829845119d8cc530ccd7df42b
sso <sso9524@naver.com>
2020-03-07 15:28:22 +0900

    번호리스트 페이지 작성

--- front-end/html/components/aside/myPage.js
+++ front-end/html/components/aside/myPage.js
@@ -4,31 +4,23 @@ document.write(`<div class="anchor-title">
 <ul class="anchor-list-container">
 
 <li>  
+<h3>번호조합리스트</h3>
     <ul class="anchor-list-box">
-    <li><a href="../myPage/myLottoList.html">번호조합리스트</a> </li>
+    <li><a href="../myPage/myNumList.html">나의 번호리스트</a> </li>
+    <li><a href="../myPage/myIncNumList.html">나의 추천번호리스트</a> </li>
+    <li><a href="../myPage/myExcNumList.html">나의 제외번호리스트</a> </li>
     </ul>
 </li>
 <li> 
+<h3>회원정보</h3>
     <ul class="anchor-list-box">
     <li><a href="#">결제내역</a> </li>
-    </ul>
-</li>
-
-<li>
-    <ul class="anchor-list-box">
-    <li><a href="#">닉네임 변경</a> </li>
-    </ul>
-</li>
-<li>
-    <ul class="anchor-list-box">
+    <li><a href="#">닉네임 변경</a> </li>   
     <li><a href="#">비밀번호 변경</a> </li>
-    </ul>
-</li>
-<li>
-    <ul class="anchor-list-box">
     <li>  <a href="#">회원탈퇴</a> </li>
     </ul>
 </li>
+
 </ul>
 
 `);
