450aa65f607ee130a13ff7e37b5a13db7d2d284a
sso <sso9524@naver.com>
2020-03-09 18:01:09 +0900

    번호리스트 토글 수정

--- front-end/src/myNumList.ts
+++ front-end/src/myNumList.ts
@@ -1,3 +1,25 @@
-const numToggle = document.getElementsByClassName('mypage-table-num-toggle');
+const numToggleBtn = document.querySelector('.mypage-table-num-toggle');
+const pastWinBox = document.querySelector('.func3-past-win-table');
+const filterNumInfo = document.querySelector('.func3-list-filter-table');
 
-console.log(numToggle);
+numToggleBtn.addEventListener('click', numToggle());
+
+function numToggle() {
+    let flag = false;
+    return function () {
+        if (!flag) {
+
+            pastWinBox.classList.remove('none');
+            filterNumInfo.classList.remove('none');
+            flag = true;
+        }
+        else {
+
+            pastWinBox.classList.add('none');
+            filterNumInfo.classList.add('none');
+            flag = false;
+        }
+    }
+}
+
+
