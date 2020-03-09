4f02582f904ed6e0b348747b688ad36f967fe5e6
pieaces <jaesung446@gmai.com>
2020-03-10 00:29:38 +0900

    freegenerator 오류수정 및 경고디자인효과

--- front-end/src/freeGenerator.ts
+++ front-end/src/freeGenerator.ts
@@ -12,7 +12,7 @@ const includeCanvas = document.getElementById('include');
 const excludeCanvas = document.getElementById('exclude');
 const wrapper = document.querySelector<HTMLElement>('.func3-num-wrapper');
 const lineGenerator = document.querySelector<HTMLElement>('.line-gen-stack-chart-container');
-const lineInput = document.querySelector<HTMLElement>('.line-gen-num-table');
+const lineInputTable = document.querySelector<HTMLElement>('.line-gen-num-table');
 const textToggleShowBox = document.querySelector<HTMLElement>('.line-gen-text-toggle-show');
 const textToggleHideBox = document.querySelector<HTMLElement>('.line-gen-text-toggle-hide');
 const explainText = document.querySelector<HTMLElement>('.line-gen-text-content');
@@ -28,7 +28,11 @@ init();
 function sum() {
     return Number(first.value) + Number(tenth.value) + Number(twentieth.value) + Number(thirtieth.value) + Number(fortieth.value);
 }
+function alertEffect(){
+    lineInputTable.style.border = "2px solid rgb(237, 59, 59)";
+}
 function info() {
+    alertEffect()
     Swal.fire({
         title: '알림',
         text: '합이 6이 넘어갈 수 없습니다.',
@@ -91,12 +95,12 @@ lineBtn.addEventListener('click', async () => {
     if (lineCheck) {
         lineBtn.style.background = '';
         lineGenerator.classList.add('none');
-        lineInput.classList.add('hide');
+        lineInputTable.classList.add('hide');
         lineCheck = false;
     } else {
         lineBtn.style.background = '#617897';
         lineGenerator.classList.remove('none');
-        lineInput.classList.remove('hide');
+        lineInputTable.classList.remove('hide');
         lineCheck = true;
         const data = await getUnAuthAPI('/stats/mass', { method: 'line' });
         console.log(data)
@@ -115,8 +119,7 @@ async function init() {
     makeLine(includeCanvas, include);
     makeLine(excludeCanvas, exclude);
     makeBtn.addEventListener('click', async () => {
-        let params: any;
-        if (!lineCheck || lineCheck && sum() === 6) {
+            if (!lineCheck || lineCheck && sum() === 6) {
             let dataSet: any;
             wrapper.classList.add('none');
             Swal.fire({
@@ -143,20 +146,57 @@ async function init() {
             footer.style.fontWeight = 'bold';
 
             if (lineCheck) {
-                params = [Number(first.value), Number(tenth.value), Number(twentieth.value), Number(thirtieth.value), Number(fortieth.value)]
-                dataSet = await getAuthAPI('/numbers/generator/free', { lineCount: JSON.stringify(params) });
+                const lineCount = [Number(first.value), Number(tenth.value), Number(twentieth.value), Number(thirtieth.value), Number(fortieth.value)];
+                const choice = makeChoice(exclude);
+                const compart = {
+                    choice: compartByLine(choice),
+                    include: compartByLine(include),
+                };
+                let choiceFlag = true;
+                let includeFlag = false;
+                for (let i = 0; i < 5; i++) {
+                    if (!compart.choice[i] && lineCount[i] || compart.choice[i] && compart.choice[i].length < lineCount[i]) {
+                        choiceFlag = false;
+                        break;
+                    }
+                    if (!includeFlag && compart.include[i] && lineCount[i] > 0) {
+                        includeFlag = doesExist(compart.choice[i], compart.include[i]);
+                    }
+                }
+                if (!choiceFlag){
+                    alertEffect();
+                    Swal.fire({
+                        title:'오류',
+                        text:'현재의 제외번호로는 조합될 수 없는 구간개수 조건입니다.',
+                        icon:'error'
+                    });
+                }
+                else if (include.length > 0 && !includeFlag){
+                    alertEffect();
+                    Swal.fire({
+                        title:'오류',
+                        text:'현재의 추천번호가 절대 나올 수 없는 구간개수 조건입니다.',
+                        icon:'error'
+                    });
+                }
+                else {
+                    lineInputTable.style.border = "";
+                    dataSet = await getAuthAPI('/numbers/generator/free', { lineCount: JSON.stringify(lineCount) });
+                    Layout3.makeNumBoard(dataSet);
+                }
             } else {
+                lineInputTable.style.border = "";
                 dataSet = await getAuthAPI('/numbers/generator/free');
+                Layout3.makeNumBoard(dataSet);
             }
-            Layout3.makeNumBoard(dataSet);
-
             CheckBoxToggle.init();
         } else {
+            alertEffect();
             Swal.fire({
                 title: '알림',
                 text: '구간개수의 합이 6이 되어야합니다',
                 icon: 'info'
-            })
+            });
         }
     });
     loading.classList.add('none');
@@ -164,11 +204,8 @@ async function init() {
 
 function lineGenTextToggleInit() {
     let flag = false;
-
     textToggleShowBox.addEventListener('click', lineGenTextToggle)
-
     textToggleHideBox.addEventListener('click', lineGenTextToggle)
-
     function lineGenTextToggle() {
         if (!flag) {
             explainText.classList.remove('none');
@@ -184,7 +221,6 @@ function lineGenTextToggleInit() {
             flag = false;
         }
     }
-
 }
 
 function makeLine(canvas: HTMLElement, numbers: number[]): void {
@@ -211,5 +247,43 @@ function makeLine(canvas: HTMLElement, numbers: number[]): void {
 // })
 
 
+function doesExist(one: number[], other: number[]) {
+    let flag = false;
+    for (let i = 0; i < one.length; i++) {
+        for (let j = 0; j < other.length; j++) {
+            if (one[i] === other[j]) {
+                flag = true;
+                break;
+            }
+        }
+    }
+    return flag;
+}
+function compartByLine(numbers) {
+    const result = new Array(5);
+    numbers.forEach(num => {
+        const index = Math.floor((num - 1) / 10);
+        if (result[index])
+            result[index].push(num);
+        else
+            result[index] = [num];
+    });
+    return result;
+}
+function makeChoice(exclude) {
+    const choice = [];
+    if (exclude.length > 0) {
+        exclude.forEach((num, index) => {
+            for (let i = exclude[index - 1] + 1 || 1; i < num; i++)
+                choice.push(i);
+        });
+        for (let i = exclude[exclude.length - 1] + 1; i <= 45; i++)
+            choice.push(i);
+    }
+    else
+        for (let i = 1; i <= 45; i++)
+            choice.push(i);
+    return choice;
+}
 
 
