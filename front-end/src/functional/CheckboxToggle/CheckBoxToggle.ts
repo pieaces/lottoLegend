d6449dbfd23aac38db788aa95ae2043f546e284c
sso <sso9524@naver.com>
2020-03-10 16:44:24 +0900

    체크박스토글 함수 수정

--- front-end/src/functional/CheckboxToggle/CheckBoxToggle.ts
+++ front-end/src/functional/CheckboxToggle/CheckBoxToggle.ts
@@ -1,15 +1,16 @@
 
-const inputbox = document.getElementsByClassName('input-checkbox-container');
 
 export default class CheckBoxToggle {
 
     static init() {
-        Array.from(inputbox).forEach((node) => {
-            const checkbox = node.children[0] as HTMLInputElement;
-            checkbox.addEventListener('click', () => {
-                const checkTextBox = node.children[1] as HTMLElement;
+
+        const inputbox = document.querySelectorAll<HTMLInputElement>('.input-checkbox-container>input');
+        inputbox.forEach((node) => {
+
+            node.addEventListener('click', () => {
+                const checkTextBox = node.nextElementSibling as HTMLElement;
                 const checkText = checkTextBox.children[0];
-                if (checkbox.checked) {
+                if (node.checked) {
                     checkText.classList.remove('none');
                     checkText.textContent = '✔';
                     checkTextBox.style.backgroundColor = '#22B8A5';
@@ -26,10 +27,11 @@ export default class CheckBoxToggle {
 
     static allCheck() {
         const allCheckBox = document.querySelector<HTMLInputElement>('#all-check');
+        const inputbox = document.querySelectorAll<HTMLInputElement>('.input-checkbox-container>input');
         allCheckBox.addEventListener('click', () => {
             if (allCheckBox.checked) {
-                Array.from(inputbox).forEach(node => {
-                    const checkTextBox = node.children[1] as HTMLElement;
+                inputbox.forEach(node => {
+                    const checkTextBox = node.nextElementSibling as HTMLElement;
                     const checkText = checkTextBox.children[0];
                     checkText.classList.remove('none');
                     checkText.textContent = '✔';
@@ -37,8 +39,8 @@ export default class CheckBoxToggle {
 
                 })
             } else {
-                Array.from(inputbox).forEach(node => {
-                    const checkTextBox = node.children[1] as HTMLElement;
+                inputbox.forEach(node => {
+                    const checkTextBox = node.nextElementSibling as HTMLElement;
                     const checkText = checkTextBox.children[0];
                     checkText.classList.add('none');
                     checkText.textContent = '';
