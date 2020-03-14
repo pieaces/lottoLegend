6e89555d1a9baa05d17aecb112d94f7df166596f
pieaces <jaesung446@gmai.com>
2020-03-14 19:15:17 +0900

    recommend SS로 변경

--- API/dynamoDB/recommend.ts
+++ API/dynamoDB/recommend.ts
@@ -5,9 +5,9 @@ import { AWSError } from "aws-sdk/lib/error";
 
 export async function updateRecommendUsers(post: number, userName: string):Promise<Response> {
     const recommendUsers = await getRecommendUsers(post);
-    const index = recommendUsers.indexOf(userName);
-    if(index !== -1){
-        await removeRecommendUser(post, index);
+    const exist = recommendUsers.some(user => user === userName);
+    if(exist){
+        await removeRecommendUser(post, userName);
         return new Response(true, "이미 추천하셨습니다.");
     }
     const params = {
@@ -16,11 +16,8 @@ export async function updateRecommendUsers(post: number, userName: string):Promi
             "#Users": 'RecommendUsers',
         },
         ExpressionAttributeValues: {
-            ":empty_list": {
-                L: new Array()
-            },
             ":element": {
-                L: [{ S: userName }]
+                SS: [userName]
             },
         },
         Key: {
@@ -28,29 +25,34 @@ export async function updateRecommendUsers(post: number, userName: string):Promi
                 N: post.toString()
             }
         },
-        UpdateExpression: `SET #Users = list_append(if_not_exists(#Users, :empty_list), :element)`
+        UpdateExpression: `ADD #Users :element`
     };
 
     return new Promise((resolve, reject) => {
-        dynamoDB.updateItem(params, (err: AWSError, data: UpdateItemOutput) => {
+        dynamoDB.updateItem(params, (err: AWSError) => {
             if (err) reject(err);
             resolve(new Response(false));
         });
     });
 }
 
-async function removeRecommendUser(post: number, index:number):Promise<void> {
+async function removeRecommendUser(post: number, userName:string):Promise<void> {
     const params = {
         TableName: 'LottoPosts',
         ExpressionAttributeNames: {
             "#Users": 'RecommendUsers',
+        },
+        ExpressionAttributeValues: {
+            ":element": {
+                SS: [userName]
+            },
         },
         Key: {
             "Id": {
                 N: post.toString()
             }
         },
-        UpdateExpression: `Remove #Users[${index}]`
+        UpdateExpression: `DELETE #Users :element`
     };
 
     return new Promise((resolve, reject) => {
