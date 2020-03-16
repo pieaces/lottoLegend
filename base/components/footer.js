d45eb7fff3a745db74bad81e68a2fbfc293780d4
sso <sso9524@naver.com>
2020-03-17 01:06:45 +0900

    프리미엄,헤더,푸터 반응형 수정

--- base/components/footer.js
+++ base/components/footer.js
@@ -1,6 +1,6 @@
 document.write(`<footer><nav class="top-footer-container">
     <div class="top-footer container">
-        <ul>
+        <ul class="top-footer-list">
             <li><a href="#">회사소개</a></li>
             <li><a href="#">이용약관</a></li>
             <li><a href="#">개인정보처리방침</a></li>
@@ -10,28 +10,22 @@ document.write(`<footer><nav class="top-footer-container">
 </nav>
 <nav class="mid-footer-container">
     <div class="mid-footer container">
-        <div>
-            <p>
-                사실을
-                <br />
-                토대로 한
-                <br/> 통계
-            </p>
-        </div>
-        <div>
-            <p>고객만족을 <br />위한 최고의
-            <br/> 서비스</p>
-        </div>
-        <div>
-            <p>다른 사이트에서 <br />찾아볼 수 없는 
-            <br/>새로운 시스템</p>
-        </div>
+        <ul class="mid-footer-list">
+        <li>  사실을
+        토대로 한
+         통계</li>
+        <li>고객만족을 위한 최고의
+         서비스</li>
+        <li> 다른 사이트에서 찾아볼 수 없는 
+       새로운 시스템</li>
+
+    </ul>
     </div>
 </nav>
 <nav class="bottom-footer-container">
     <div class="bottom-footer container">
         <h1>로또끝</h1>
-        <div>
+        <div class="bottom-footer-content">
             <p>
                 당사의 분석시스템은 누적패턴을 분석/필터링한 정보제공만을
                 목적으로 하며, 당첨확률 개선서비스가 아니므로 서비스 이용
@@ -39,13 +33,14 @@ document.write(`<footer><nav class="top-footer-container">
                 최종책임은 서비스 이용자 본인에게 있습니다.
             </p>
             <p>
-                법인명:(주)브레인콘텐츠대표자:강진원 장대용
+              <span>  법인명:(주)브레인콘텐츠대표자:강진원 장대용</span>
+                <span>
                 도로명주소:서울특별시 강남구 테헤란로 325 (역삼동) 어반벤치빌딩
-                14층
+                14층</span>
             </p>
             <p>
-                TEL:1588-0649 FAX:02-2017-7994 E-mail:help@brain-group.co.kr
-                제휴문의:lottorichmkt@gmail.com
+            <span>TEL:1588-0649 FAX:02-2017-7994</span> <span>E-mail:help@brain-group.co.kr</span>
+                <span>제휴문의:lottorichmkt@gmail.com</span>
             </p>
             <p>
                 통신판매업신고:2017-서울강남-00217호 사업자
