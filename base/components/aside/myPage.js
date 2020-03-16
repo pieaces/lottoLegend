8c9ed5dad6ede67cbd6dc42ce58c31c817e598f7
sso <sso9524@naver.com>
2020-03-16 17:13:59 +0900

    변경 페이지 작성 중

--- base/components/aside/myPage.js
+++ base/components/aside/myPage.js
@@ -4,22 +4,22 @@ document.write(`<div class="anchor-title">
 <ul class="anchor-list-container">
 <li>  
     <ul class="anchor-list-box">
-    <li><a href="home.html">마이 페이지 홈</a> </li>
+    <li><a href="/myPage/home.html">마이 페이지 홈</a> </li>
     
     </ul>
 </li>
 <li>  
 <h3>번호목록</h3>
     <ul class="anchor-list-box">
-    <li><a href="numbersList.html">나의 번호리스트</a> </li>
-    <li><a href="IncludeExclude.html">나의 추천/제외 번호</a> </li>
+    <li><a href="/myPage/numbersList.html">나의 번호리스트</a> </li>
+    <li><a href="/myPage/IncludeExclude.html">나의 추천/제외 번호</a> </li>
     </ul>
 </li>
 <li> 
 <h3>회원정보</h3>
     <ul class="anchor-list-box">
     <li><a href="#">결제내역</a> </li>
-    <li><a href="#">닉네임 변경</a> </li>   
+    <li><a href="/account/modifyNickname.html">닉네임 변경</a> </li>   
     <li><a href="#">비밀번호 변경</a> </li>
     <li>  <a href="#">회원탈퇴</a> </li>
     </ul>
