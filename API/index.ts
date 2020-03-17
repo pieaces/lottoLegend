203dbe4ee230b1f6232ff2dac70678560d4e6881
pieaces <jaesung446@gmai.com>
2020-03-17 19:24:06 +0900

    name trim 처리

--- API/index.ts
+++ API/index.ts
@@ -2,17 +2,23 @@ import createUser from "./dynamo/createUser";
 import Users from "./mariaDB/Users";
 
 exports.handler = async (event: any, context: any) => {
-    const nickName = event.request.userAttributes.nickname;
-    const userName = event.userName;
+    const nickName = event.request.userAttributes.nickname.trim();
+    const userName = event.userName.trim();
 
     const userDB = new Users();
     const users = await userDB.getNickNames() as { nickName: string }[];
     if (users.some(user => user.nickName === nickName)) {
-        context.fail("이미 존재하는 닉네임입니다.");
+        return context.fail("이미 존재하는 닉네임입니다.");
     }
 
-    await createUser(userName);
-    await userDB.create(userName, nickName);
-    event.response.autoConfirmUser = true;
-    context.succeed(event);
+    try {
+        await createUser(userName);
+        await userDB.create(userName, nickName);
+        event.response.autoConfirmUser = true;
+        return context.succeed(event);
+    } catch (err) {
+        if (err.code === "ConditionalCheckFailedException") {
+            return context.fail('이미 존재하는 아이디입니다.');
+        }
+    }
 };
