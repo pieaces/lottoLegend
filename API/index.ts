10d45777861d5f2e53c2f55fb7c73eb5388ebab5
pieaces <jaesung446@gmai.com>
2020-03-17 19:36:40 +0900

    message . 뺌

--- API/index.ts
+++ API/index.ts
@@ -8,7 +8,7 @@ exports.handler = async (event: any, context: any) => {
     const userDB = new Users();
     const users = await userDB.getNickNames() as { nickName: string }[];
     if (users.some(user => user.nickName === nickName)) {
-        return context.fail("이미 존재하는 닉네임입니다.");
+        return context.fail("이미 존재하는 닉네임입니다");
     }
 
     try {
@@ -18,7 +18,7 @@ exports.handler = async (event: any, context: any) => {
         return context.succeed(event);
     } catch (err) {
         if (err.code === "ConditionalCheckFailedException") {
-            return context.fail('이미 존재하는 아이디입니다.');
+            return context.fail('이미 존재하는 아이디입니다');
         }
     }
 };
