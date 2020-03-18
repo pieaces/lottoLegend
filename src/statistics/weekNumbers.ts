ccdfdd1e6c4cb5cb6f229fb3a85796e3d27ffd6f
pieaces <jaesung446@gmai.com>
2020-03-18 15:23:28 +0900

    금주의 번호 완료

--- src/statistics/weekNumbers.ts
+++ src/statistics/weekNumbers.ts
@@ -1,72 +1,39 @@
 import Selectr, { IOptions } from 'mobius1-selectr';
-import { setColorLotto } from '../functions/index';
-import { headerSign, getIdToken } from '../amplify/auth';
+import { setColorLotto, networkAlert } from '../functions/index';
+import { headerSign } from '../amplify/auth';
 import configure from '../amplify/configure';
+import { getUnAuthAPI } from '../amplify/api';
 
 const selectBox = document.querySelector<HTMLSelectElement>('#exc-num-week-select-box');
-const excNumWeekWrapper = document.querySelector('.exc-num-week-wrapper');
+const excNumWeekWrapper = document.querySelector<HTMLElement>('.exc-num-week-wrapper');
 
 configure();
-getIdToken();
 headerSign();
 
-const dataSetCurrent = [];
-const round = "901";
-const numbers = [];
-const hitsArr = [];
+getUnAuthAPI('/numbers/week')
+    .then(({data, rounds}) => {
+        console.log(data, rounds);
+        makeTable(data);
 
-for (let i = 0; i < 10; i++) {
-    numbers.push(i + 5);
-    hitsArr.push(false);
-}
+        const config: IOptions = {
+            data: rounds.map((round: string) => {
+                return {
+                    text: round,
+                    value: round
+                }
+            }),
+        };
+        const selector = new Selectr(selectBox, config);
+        selector.on('selectr.change', async (option) => {
+            const {data, rounds} = await getUnAuthAPI('/numbers/week/'+option.value);
+            console.log(data, rounds);
+            makeTable(data);
+        });
+    }).catch(err => networkAlert());
 
-const obj = {
-    rounds: round,
-    hitsArr: hitsArr,
-    numbers: numbers
-}
-dataSetCurrent.push(obj);
-
-const dataSet = [];
-for (let i = 0; i < 2; i++) {
-    const round = "901";
-    const numbers = [];
-    const hitsArr = [];
-
-    for (let i = 0; i < 10; i++) {
-        numbers.push(i + 5);
-        hitsArr.push(false);
-    }
-
-    const obj = {
-        rounds: round,
-        hitsArr: hitsArr,
-        numbers: numbers
-    }
-    dataSet.push(obj);
-}
-
-const rounds = ['1', '2', '3', '4', '5'];
-
-makeTable(dataSetCurrent, 'current');
-makeTable(dataSet, 'past');
-
-const config: IOptions = {
-    data: rounds.map((round: string) => {
-        return {
-            text: round,
-            value: round
-        }
-    }),
-};
-const selector = new Selectr(selectBox, config);
-selector.on('selectr.change', async (option) => {
-});
-
-
-
-function makeTable(dataSet: ({ rounds: string, numbers: number[], hitsArr: boolean[] })[], isCurrent: string) {
-    for (let k = 0; k < dataSet.length; k++) {
+function makeTable(dataSet: ({ round: string, numbers: number[], hits?: boolean[] })[]) {
+    excNumWeekWrapper.innerHTML = '';
+    for (let i = 0; i < dataSet.length; i++) {
         const div = document.createElement('div');
         div.classList.add('lotto-num-container');
 
@@ -78,26 +45,25 @@ function makeTable(dataSet: ({ rounds: string, numbers: number[], hitsArr: boole
         titleTr.classList.add('lotto-num-table-title');
 
         const th = document.createElement('th');
-        th.setAttribute('colspan', `${dataSet[k].numbers.length}`);
-        th.textContent = `${dataSet[k].rounds}회`;
+        th.setAttribute('colspan', dataSet[i].numbers.length.toString());
+        th.textContent = `${dataSet[i].round}회`;
 
         titleTr.appendChild(th);
         thead.appendChild(titleTr);
         table.appendChild(thead);
 
         const tbody = document.createElement('tbody');
-
         const NumberTr = document.createElement('tr');
 
-        for (let j = 0; j < dataSet[k].numbers.length; j++) {
+        for (let j = 0; j < dataSet[i].numbers.length; j++) {
             const td = document.createElement('td');
             const numBox = document.createElement('div');
             numBox.classList.add('lotto-num');
 
             const num = document.createElement('div');
             num.classList.add('lotto-num-value');
-            num.textContent = dataSet[k].numbers[j].toString();
-            setColorLotto(dataSet[k].numbers[j], num);
+            num.textContent = dataSet[i].numbers[j].toString();
+            setColorLotto(dataSet[i].numbers[j], num);
 
             numBox.appendChild(num);
             td.appendChild(numBox);
@@ -105,18 +71,26 @@ function makeTable(dataSet: ({ rounds: string, numbers: number[], hitsArr: boole
         }
 
         tbody.appendChild(NumberTr);
-        if (isCurrent === "past") {
+
+        if (!dataSet[i].hits) {
+            table.appendChild(tbody);
+            div.appendChild(table);
+            excNumWeekWrapper.appendChild(div);
+            const boundary = document.createElement('div');
+            boundary.classList.add('num-list-boundary');
+            excNumWeekWrapper.appendChild(boundary);
+        } else {
             div.classList.add('lotto-num-container-past');
             let hitsTotal = 0;
             const hitsTr = document.createElement('tr');
 
-            for (let j = 0; j < dataSet[k].hitsArr.length; j++) {
+            for (let j = 0; j < dataSet[i].hits.length; j++) {
                 const td = document.createElement('td');
-                if (dataSet[k].hitsArr[j] === true) {
+                if (dataSet[i].hits[j]) {
                     td.textContent = "적중";
                     hitsTotal++;
                 } else {
-                    td.textContent = "적중 x";
+                    td.textContent = "-";
                 }
                 hitsTr.appendChild(td);
             }
@@ -127,20 +101,14 @@ function makeTable(dataSet: ({ rounds: string, numbers: number[], hitsArr: boole
             const resultTr = document.createElement('tr');
             const resultTd = document.createElement('td');
 
-            resultTd.setAttribute('colspan', `${dataSet[k].numbers.length}`);
-            resultTd.innerHTML = `${dataSet[k].numbers.length}개 중 <span class="exc-num-week-hit">${hitsTotal}개</span> 적중`;
+            resultTd.setAttribute('colspan', dataSet[i].numbers.length.toString());
+            resultTd.innerHTML = `${dataSet[i].numbers.length}개 중 <span class="exc-num-week-hit">${hitsTotal}개</span> 적중 (${Math.round(hitsTotal/dataSet[i].numbers.length*100)}%)`;
             resultTr.appendChild(resultTd);
             tfoot.appendChild(resultTr);
             table.appendChild(tfoot);
-        }
-
-        table.appendChild(tbody);
-        div.appendChild(table);
-        excNumWeekWrapper.appendChild(div);
-        if (isCurrent === 'current') {
-            const boundary = document.createElement('div');
-            boundary.classList.add('num-list-boundary');
-            excNumWeekWrapper.appendChild(boundary);
+            table.appendChild(tbody);
+            div.appendChild(table);
+            excNumWeekWrapper.appendChild(div);
         }
     }
 }
