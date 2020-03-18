72c6e42ff7b1ddca66541e39c672ca4e22f41700
pieaces <jaesung446@gmai.com>
2020-03-19 01:06:40 +0900

    generator loading 수정

--- src/system/generatorLoading/index.ts
+++ src/system/generatorLoading/index.ts
@@ -1,28 +1,35 @@
 import Swal from "sweetalert2";
 
-export default function generatorLoading(time:number) {
+export default function generatorLoading(time: number) {
     const wrapper = document.querySelector<HTMLElement>('.func3-num-wrapper');
     wrapper.classList.add('none');
+    let timerInterval: any;
     Swal.fire({
-        title: `<div class="lds-circle"><div></div></div>`,
-        footer: '주어진 데이터로 고객님의 번호를 조합중입니다',
+        title: '주어진 데이터로 번호를 조합중입니다',
+        html: '데이터 처리중 <b></b> ms',
+        timer: time - Math.round(Math.random()*750),
+        timerProgressBar: true,
         allowOutsideClick: false,
-        timer: time,
-    }).then(() => {
+        onBeforeOpen: () => {
+            Swal.showLoading()
+            timerInterval = setInterval(() => {
+                const content = Swal.getContent()
+                if (content) {
+                    const b = content.querySelector('b')
+                    if (b) {
+                        b.textContent = Swal.getTimerLeft().toString()
+                    }
+                }
+            }, 100)
+        },
+        onClose: () => {
+            clearInterval(timerInterval)
+        }
+    }).then((result) => {
+        /* Read more about handling dismissals below */
+        if (result.dismiss === Swal.DismissReason.timer) {
+            console.log('I was closed by the timer')
+        }
         wrapper.classList.remove('none');
     });
-    document.querySelector('.swal2-actions').innerHTML = '';
-    const popup = document.querySelector<HTMLElement>('.swal2-popup');
-    popup.style.width = "45rem";
-    popup.style.height = "27.8rem";
-    popup.style.background = 'gold';
-    popup.style.boxShadow = "0 1px 2px rgba(0,0,0,0.07),0 2px 4px rgba(0,0,0,0.07),0 4px 8px rgba(0,0,0,0.07),0 8px 16px rgba(0,0,0,0.07),0 16px 32px rgba(0,0,0,0.07),0 32px 64px rgba(0,0,0,0.07)";
-    const backdrop = document.querySelector<HTMLElement>('.swal2-backdrop-show')
-    backdrop.style.background = "rgba(0, 0, 0, 0.6)";
-    backdrop.style.boxShadow = "rgba(0, 0, 0, 0.7) 19rem 13rem 24rem inset, rgba(0, 0, 0, 0.7) -14rem -9rem 23rem inset";
-    const footer = document.querySelector<HTMLElement>('.swal2-footer');
-    footer.style.margin = '0';
-    footer.style.padding = '1.5em 0 0';
-    footer.style.fontSize = '2rem';
-    footer.style.fontWeight = 'bold';
 }
