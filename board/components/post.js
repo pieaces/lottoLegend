f341748a7e9c3755938935ec8415ff2e1f61b87e
sso <sso9524@naver.com>
2020-03-18 14:09:36 +0900

    post 반응형 작성

--- board/components/post.js
+++ board/components/post.js
@@ -10,13 +10,18 @@ document.write(`
 <div id="image-wrapper" class="image-list">
     <div class="file-list-info">
         <div class="button-wrapper">
+            <div class="img-btn-container">
             <div class="img-btn-box">
-                <span class="label">
+                <div class="img-upload-box">
+                <span class="img-upload-text">
                     사진 업로드
                 </span>
                 <input type="file" class="files-upload" id="files-upload" accept="image/*" multiple="multiple" />
+                </div>
                 <button class="btn image-remove" id="image-remove" disabled>사진 삭제</button>
+                     
                 <span class="image-total-size" id="image-total-size">0.00MB</span> <span class="post-alert" id="post-alert">최대 4MB까지 업로드 가능합니다.</span>
+                </div>
             </div>
             <button type="submit" class="btn post-btn" id="submit-btn">
                 완료
