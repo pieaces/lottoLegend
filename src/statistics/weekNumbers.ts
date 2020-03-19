43234c096adf703c1cadf2817ef3b4d3966e683b
sso <sso9524@naver.com>
2020-03-19 19:24:59 +0900

    금주의 제외번호 수정 중

--- src/statistics/weekNumbers.ts
+++ src/statistics/weekNumbers.ts
@@ -6,12 +6,58 @@ import { getUnAuthAPI } from '../amplify/api';
 
 const selectBox = document.querySelector<HTMLSelectElement>('#exc-num-week-select-box');
 const excNumWeekWrapper = document.querySelector<HTMLElement>('.exc-num-week-wrapper');
+const lottoNumTable = document.getElementsByClassName('lotto-num-table');
+const mqMobile = window.matchMedia("(max-width: 767px)");
+
+mqMobile.addListener(mqHeaderFunc);
+
+function mqHeaderFunc(mediaQuery) {
+    if (mediaQuery.matches) {
+        for (let i = 0; i < lottoNumTable.length; i++) {
+            if (lottoNumTable[i].children[1].tagName === "tbody") {
+                lottoNumTable[i].children[1].firstElementChild.children[4].innerHTML += "</tr><tr>";
+            }
+        }
+    } else {
+        for (let i = 0; i < lottoNumTable.length; i++) {
+            const chilrenEl = lottoNumTable[i].children[1];
+            if (chilrenEl.tagName === "tbody") {
+                const numTr1 = document.createElement('tr');
+                const hitsTr1 = document.createElement('tr');
+                const numTr2 = document.createElement('tr');
+                const hitsTr2 = document.createElement('tr');
+                for (let j = 0; j < chilrenEl.children[0].children.length; j++) {
+                    if (j === 4) {
+                        numTr1.appendChild(chilrenEl.children[0].children[j]);
+                    }
+                    if (j === chilrenEl.children[0].children.length - 1) {
+                        numTr2.appendChild(chilrenEl.children[0].children[j]);
+                    }
+                }
+                for (let j = 0; j < chilrenEl.children[1].children.length; j++) {
+                    if (j === 4) {
+                        hitsTr1.appendChild(chilrenEl.children[1].children[j]);
+                    }
+                    if (j === chilrenEl.children[1].children.length - 1) {
+                        hitsTr2.appendChild(chilrenEl.children[1].children[j]);
+                    }
+                }
+                chilrenEl.innerHTML = '';
+                chilrenEl.appendChild(numTr1);
+                chilrenEl.appendChild(hitsTr1);
+                chilrenEl.appendChild(numTr2);
+                chilrenEl.appendChild(hitsTr2);
+
+            }
+        }
+    }
+}
 
 configure();
 headerSign();
 
 getUnAuthAPI('/numbers/week')
-    .then(({data, rounds}) => {
+    .then(({ data, rounds }) => {
         makeTable(data);
 
         const config: IOptions = {
@@ -24,7 +70,7 @@ getUnAuthAPI('/numbers/week')
         };
         const selector = new Selectr(selectBox, config);
         selector.on('selectr.change', async (option) => {
-            const {data} = await getUnAuthAPI('/numbers/week/'+option.value);
+            const { data } = await getUnAuthAPI('/numbers/week/' + option.value);
             makeTable(data);
         });
     }).catch(err => networkAlert());
@@ -74,9 +120,9 @@ function makeTable(dataSet: ({ round: string, numbers: number[], hits?: boolean[
             table.appendChild(tbody);
             div.appendChild(table);
             excNumWeekWrapper.appendChild(div);
-            const boundary = document.createElement('div');
-            boundary.classList.add('num-list-boundary');
-            excNumWeekWrapper.appendChild(boundary);
+            const adBox = document.createElement('div');
+            adBox.classList.add('ad-box', 'box-color');
+            excNumWeekWrapper.appendChild(adBox);
         } else {
             div.classList.add('lotto-num-container-past');
             let hitsTotal = 0;
@@ -100,11 +146,11 @@ function makeTable(dataSet: ({ round: string, numbers: number[], hits?: boolean[
             const resultTd = document.createElement('td');
 
             resultTd.setAttribute('colspan', dataSet[i].numbers.length.toString());
-            resultTd.innerHTML = `${dataSet[i].numbers.length}개 중 <span class="exc-num-week-hit">${hitsTotal}개</span> 적중 (${Math.round(hitsTotal/dataSet[i].numbers.length*100)}%)`;
+            resultTd.innerHTML = `${dataSet[i].numbers.length}개 중 <span class="exc-num-week-hit">${hitsTotal}개</span> 적중 (${Math.round(hitsTotal / dataSet[i].numbers.length * 100)}%)`;
             resultTr.appendChild(resultTd);
             tfoot.appendChild(resultTr);
-            table.appendChild(tfoot);
             table.appendChild(tbody);
+            table.appendChild(tfoot);
             div.appendChild(table);
             excNumWeekWrapper.appendChild(div);
         }
