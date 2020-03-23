85741dde54bc62dd647aef7787cb6d4676729f23
sso <sso9524@naver.com>
2020-03-23 19:18:23 +0900

    헤더 js 작업, 통계자료 드롭다운 작업

--- base/components/header.js
+++ base/components/header.js
@@ -1,4 +1,84 @@
-document.write(` <header>
+// document.write(` <header>
+// <nav class="top-nav-container">
+//     <div class="container top-nav">
+//         <div class="top-nav-right">
+//             <ul>
+//                 <li id="header-signIn" class="none"><a href="/account/signIn.html">로그인</a></li>
+//                 <li id="header-signUp" class="none"><a href="/account/agree.html">회원가입</a></li>
+//                 <li id="header-signOut" class="none"><a>로그아웃</a></li>
+//                 <li id="header-myPage" class="none"><a href="/myPage/home.html">마이페이지</a></li>
+//             </ul>
+//         </div>
+//     </div>
+// </nav>
+// <nav class="mid-nav-container">
+//     <div class="hover-menu-container none">
+//         <div class="hover-menu-box container">
+//             <ul>
+//             </ul>
+//             <ul></ul>
+//             <ul>
+//         <li> <a href="/system/experience.html">체험하기</a> </li>
+//         <li> <a href="/system/include.html">추천/제외수 생성기</a> </li>
+//         <li> <a href="/system/basic.html">번호 조합기</a> </li>
+//             </ul>
+//             <ul>
+//                 <li> <a href="/statistics/weekNumbers.html">금주의 제외번호</a> </li>
+//                 <li> <a href="/board/analysis/list.html">공유 게시판</a> </li>
+//                 <li> <a href="/statistics/winNumbers.html">통계자료</a> </li>
+//                 </ul>
+//             <ul>
+//                 <li> <a href="/board/notice/list.html">공지사항</a> </li>
+//                 <li> <a href="/board/win/list.html">당첨인증 게시판</a> </li>
+//                 <li> <a href="/board/free/list.html">자유게시판</a> </li>
+//             </ul>
+//             <ul>
+//                 <li> <a href="/board/qna/oftenAskList.html">자주 묻는 질문</a> </li>
+//                 <li> <a href="/board/qna/list.html">Q & A</a> </li>
+//             </ul>
+//         </div>
+//     </div>
+//     <div class="container mid-nav">
+//         <ul class="mid-nav-menu">
+//             <li>
+//                 <a href="/main.html">
+//                   로또끝
+//                 </a>
+//             </li>
+//             <li><a href="#">소개</a>
+
+//             </li>
+//             <li> <a href="#">시스템</a> </li>
+//             <li> <a href="#">분석실</a> </li>
+
+//             <li> <a href="#">커뮤니티</a> </li>
+//             <li><a href="#">고객문의</a></li>
+//         </ul>
+//     </div>
+// </nav>
+// <nav class="bottom-nav-container">
+//     <div class="container bottom-nav">
+//         <div class="bottom-nav-left">
+//             <ul>
+//                 <li>공지</li>
+//                 <li>
+//                     [831회 11억 실제 1등 당첨자 탄생] 역대 90번째 실제1등! 2주연속 1등
+//                     조합배출!
+//                 </li>
+//             </ul>
+//         </div>
+//         <div class="bottom-nav-right">
+//             <p>캔버스로또만의 시스템을 이용해보시기 바랍니다</p>
+//         </div>
+//     </div>
+// </nav>
+// <div class="loading-box none">
+// <div class="loading"><div></div><div></div><div></div><div></div></div>
+// </div>
+// </header>
+// `);
+
+document.write(`     <header>
 <nav class="top-nav-container">
     <div class="container top-nav">
         <div class="top-nav-right">
@@ -12,21 +92,54 @@ document.write(` <header>
     </div>
 </nav>
 <nav class="mid-nav-container">
+    <div class="click-menu-container">
+        <ul class="click-menu-box click-menu-system none">
+            <li> <a href="/system/premium.html">체험하기</a> </li>
+            <li> <a href="/system/include.html">추천수</a>
+            <li> <a href="/system/exclude.html">제외수</a> </li>
+            <li> <a href="/system/basic.html">기본 필터</a> </li>
+            <li> <a href="/system/premium.html">프리미엄 필터</a> </li>
+        </ul>
+        <ul class="click-menu-box click-menu-statistics none">
+            <li> <a href="/statistics/weekNumbers.html">금주의 제외번호</a> </li>
+            <li> <a href="/board/analysis/list.html">분석 Tip 공유</a> </li>
+            <li><a href="/board/include/list.html">추천수 공유</a></li>
+            <li><a href="/board/exclude/list.html">제외수 공유</a></li>
+            <li><a href="/statistics/winNumbers.html">통계 자료</a></li>
+        </ul>
+        <ul class="click-menu-box click-menu-community none">
+            <li><a href="/board/notice/list.html">공지사항</a></li>
+            <li><a href="/board/win/list.html">당첨인증 게시판</a></li>
+            <li><a href="/board/free/list.html">자유게시판</a> </li>
+        </ul>
+        <ul class="click-menu-box click-menu-qna none">
+            <li><a href="/board/qna/oftenAskList.html">자주 묻는 질문</a></li>
+            <li> <a href="/board/qna/list.html">Q & A</a> </li>
+        </ul>
+        <ul class="click-menu-box click-menu-mypage none">
+            <li><a href="/myPage/home.html">마이 페이지 홈</a> </li>
+            <li><a href="/myPage/numbersList.html">나의 번호리스트</a> </li>
+            <li><a href="/myPage/IncludeExclude.html">나의 추천/제외 번호</a> </li>
+            <li><a href="#">결제내역</a> </li>
+        </ul>
+        <div class="click-menu-toggle none">더보기</div>
+    </div>
+
     <div class="hover-menu-container none">
         <div class="hover-menu-box container">
             <ul>
             </ul>
             <ul></ul>
             <ul>
-        <li> <a href="/system/experience.html">체험하기</a> </li>
-        <li> <a href="/system/include.html">추천/제외수 생성기</a> </li>
-        <li> <a href="/system/basic.html">번호 조합기</a> </li>
+                <li> <a href="/system/experience.html">체험하기</a> </li>
+                <li> <a href="/system/include.html">추천/제외수 생성기</a> </li>
+                <li> <a href="/system/basic.html">번호 조합기</a> </li>
             </ul>
             <ul>
                 <li> <a href="/statistics/weekNumbers.html">금주의 제외번호</a> </li>
                 <li> <a href="/board/analysis/list.html">공유 게시판</a> </li>
                 <li> <a href="/statistics/winNumbers.html">통계자료</a> </li>
-                </ul>
+            </ul>
             <ul>
                 <li> <a href="/board/notice/list.html">공지사항</a> </li>
                 <li> <a href="/board/win/list.html">당첨인증 게시판</a> </li>
@@ -42,7 +155,7 @@ document.write(` <header>
         <ul class="mid-nav-menu">
             <li>
                 <a href="/main.html">
-                  로또끝
+                    로또끝
                 </a>
             </li>
             <li><a href="#">소개</a>
@@ -50,7 +163,7 @@ document.write(` <header>
             </li>
             <li> <a href="#">시스템</a> </li>
             <li> <a href="#">분석실</a> </li>
-            
+
             <li> <a href="#">커뮤니티</a> </li>
             <li><a href="#">고객문의</a></li>
         </ul>
@@ -73,7 +186,11 @@ document.write(` <header>
     </div>
 </nav>
 <div class="loading-box none">
-<div class="loading"><div></div><div></div><div></div><div></div></div>
+    <div class="loading">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
 </div>
-</header>
-`);
+</header>`);
