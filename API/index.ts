32bed21f746d63f53a18cee43efd3d6bdd1f02a4
pieaces <jaesung446@gmai.com>
2020-04-01 00:44:14 +0900

    autoputlotto 제거

--- API/index.ts
+++ API/index.ts
@@ -2,22 +2,16 @@ import autoPutSecond from "./dynamo/autoPutSecond";
 import autoDelete from "./dynamo/autoDelete";
 import autoRank from "./dynamo/autoRank";
 import counterLotto, { getCurrentRound } from "./dynamo/functions";
-import lambda from "./lambda";
 
 exports.handler = async () => {
-    const params = {
-        FunctionName: 'lotto-auto-put-lotto',
-    };
-    lambda.invoke(params, function (err) {
-        if (err) console.log(err);
-    });
-
     const total = getCurrentRound();
     const count = await counterLotto() - 1;
     if (count < total) {
         await autoPutSecond();
         await autoDelete();
         await autoRank();
+    }else{
+        console.log('최신화 상태');
     }
     console.log('데이터 자동화 시스템 종료');
 };
--- API/lambda/index.ts
+++
@@ -1,4 +0,0 @@
-import Lambda from 'aws-sdk/clients/lambda'
-const lambda = new Lambda();
-
-export default lambda;
