811b8d75782a974d2925149f45077137f06ba1ea
sso <sso9524@naver.com>
2020-03-31 22:40:25 +0900

    결제 페이지 디자인 수정

---
+++ src/introduce/pay.ts
@@ -0,0 +1,18 @@
+const priceCheckbox = document.querySelectorAll<HTMLInputElement>('.price-checkbox');
+
+checkboxToggle();
+
+function checkboxToggle() {
+    let current = null;
+    for (let i = 0; i < priceCheckbox.length; i++) {
+        priceCheckbox[i].addEventListener('click', () => {
+            if (current !== null) {
+                if (current !== i) {
+                    priceCheckbox[current].checked = false;
+                }
+            }
+            current = i;
+        })
+    }
+
+}
