088f7e7517c49d1f35058ea9a9f3fcec5d61b5b6
pieaces <jaesung446@gmai.com>
2020-04-01 18:48:33 +0900

    analytics

--- src/system/experience.ts
+++ src/system/experience.ts
@@ -6,6 +6,42 @@ import { headerSign } from '../amplify/auth';
 configure();
 headerSign();
 
+import Analytics from '@aws-amplify/analytics';
+//Record a custom event
+Analytics.record({
+    name: 'Album',
+    attributes: { genre: 'Rock', year: '1989' }
+}).then(value => console.log(value));
+
+Analytics.autoTrack('pageView', {
+    // REQUIRED, turn on/off the auto tracking
+    enable: true,
+    // OPTIONAL, the event name, by default is 'pageView'
+    eventName: 'pageView',
+    // OPTIONAL, the attributes of the event, you can either pass an object or a function 
+    // which allows you to define dynamic attributes
+    attributes: {
+        attr: 'attr'
+    },
+    // when using function
+    // attributes: () => {
+    //    const attr = somewhere();
+    //    return {
+    //        myAttr: attr
+    //    }
+    // },
+    // OPTIONAL, by default is 'multiPageApp'
+    // you need to change it to 'SPA' if your app is a single-page app like React
+    type: 'multiPageApp',
+    // OPTIONAL, the service provider, by default is the AWS Pinpoint
+    provider: 'AWSPinpoint',
+    // OPTIONAL, to get the current page url
+    getUrl: () => {
+        // the default function
+        return window.location.origin + window.location.pathname;
+    }
+});
+
 const loading = document.querySelector<HTMLElement>('.loading-box');
 loading.classList.remove('none');
 const layout = new Layout(DataAPI.getInstance() as IDataAPI);
