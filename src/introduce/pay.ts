2f369b9f243eff0590754c8e79991b1e60007dba
sso <sso9524@naver.com>
2020-04-01 18:33:16 +0900

    결제 페이지 카드 디자인 수정

--- src/introduce/pay.ts
+++ src/introduce/pay.ts
@@ -1,47 +1,64 @@
 const priceCheckbox = document.querySelectorAll<HTMLInputElement>('.price-checkbox');
 const priceContainer = document.querySelectorAll<HTMLElement>('.price-container');
-const cardHoverBox = document.querySelectorAll<HTMLElement>('.price-service-anchor-hover-box');
+const priceBox = document.querySelectorAll<HTMLElement>('.price-box');
+const priceAnchorBox = document.querySelectorAll<HTMLElement>('.price-service-anchor-box');
 
 checkboxToggle();
 
 function checkboxToggle() {
     let current = null;
-    const overEventHandler: (() => void)[] = [];
-    const outEventHandler: (() => void)[] = [];
+    const frontOverEventHandler: (() => void)[] = [];
+    const frontOutEventHandler: (() => void)[] = [];
+    const backOverEventHandler: (() => void)[] = [];
+    const backOutEventHandler: (() => void)[] = [];
     for (let i = 0; i < priceContainer.length; i++) {
         priceContainer[i].addEventListener('click', function () {
             if (current === null) {
-                priceContainer[i].classList.add("cardRotate");
-                cardHoverBox[i].removeEventListener('mouseover', overEventHandler[i]);
-                cardHoverBox[i].removeEventListener('mouseout', outEventHandler[i]);
-                cardHoverBox[i].style.opacity = "0";
+                priceContainer[i].removeEventListener('mouseover', frontOverEventHandler[i]);
+                priceContainer[i].removeEventListener('mouseout', frontOutEventHandler[i]);
+                priceContainer[i].removeEventListener('mouseover', backOverEventHandler[i]);
+                priceContainer[i].removeEventListener('mouseout', backOutEventHandler[i]);
+                priceAnchorBox[i].style.backgroundColor = "rgba(0,0,0,0.8)";
             } else {
                 if (current !== i) {
-                    priceContainer[current].classList.remove("cardRotate");
-                    priceContainer[current].classList.add("backRotate");
-                    priceContainer[i].classList.remove('backRotate');
-                    priceContainer[i].classList.add("cardRotate");
-                    cardHoverBox[current].addEventListener('mouseover', overEventHandler[current]);
-                    cardHoverBox[current].addEventListener('mouseout', outEventHandler[current]);
-                    cardHoverBox[current].style.opacity = "0";
-                    cardHoverBox[i].removeEventListener('mouseover', overEventHandler[i]);
-                    cardHoverBox[i].removeEventListener('mouseout', outEventHandler[i]);
-                    cardHoverBox[i].style.opacity = "0";
+                    priceContainer[current].addEventListener('mouseover', frontOverEventHandler[current]);
+                    priceContainer[current].addEventListener('mouseout', frontOutEventHandler[current]);
+                    priceContainer[current].addEventListener('mouseover', backOverEventHandler[current]);
+                    priceContainer[current].addEventListener('mouseout', backOutEventHandler[current]);
+                    priceBox[current].style.transform = "perspective(1000px) rotateY(0deg)";
+                    priceAnchorBox[current].style.transform = "perspective(1200px) rotateY(180deg)";
+                    priceAnchorBox[current].style.backgroundColor = "rgba(0,0,0,0.4)";
+                    priceContainer[i].removeEventListener('mouseover', frontOverEventHandler[i]);
+                    priceContainer[i].removeEventListener('mouseout', frontOutEventHandler[i]);
+                    priceContainer[i].removeEventListener('mouseover', backOverEventHandler[i]);
+                    priceContainer[i].removeEventListener('mouseout', backOutEventHandler[i]);
+                    priceAnchorBox[i].style.backgroundColor = "rgba(0,0,0,0.8)";
                 }
             }
             current = i;
         });
 
-        const overEvent = () => {
-            cardHoverBox[i].style.opacity = "1";
+        const frontOverEvent = () => {
+            priceBox[i].style.transform = "perspective(1200px) rotateY(-179.9deg)";
         }
-        overEventHandler.push(overEvent);
-        cardHoverBox[i].addEventListener('mouseover', overEvent);
-        const outEvent = () => {
-            cardHoverBox[i].style.opacity = "0";
+        frontOverEventHandler.push(frontOverEvent);
+        priceContainer[i].addEventListener('mouseover', frontOverEvent);
+        const frontOutEvent = () => {
+            priceBox[i].style.transform = "perspective(1000px) rotateY(0deg)";
         }
-        outEventHandler.push(outEvent);
-        cardHoverBox[i].addEventListener('mouseout', outEvent)
+        frontOutEventHandler.push(frontOutEvent);
+        priceContainer[i].addEventListener('mouseout', frontOutEvent);
+
+        const backOverEvent = () => {
+            priceAnchorBox[i].style.transform = "perspective(1200px) rotateY(0)";
+        }
+        backOverEventHandler.push(backOverEvent);
+        priceContainer[i].addEventListener('mouseover', backOverEvent);
+        const backOutEvent = () => {
+            priceAnchorBox[i].style.transform = "perspective(1200px) rotateY(180deg)";
+        }
+        backOutEventHandler.push(backOutEvent);
+        priceContainer[i].addEventListener('mouseout', backOutEvent);
     }
 }
 
