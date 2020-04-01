ed4921702891d4a73ebfbf003ce23173624af65f
sso <sso9524@naver.com>
2020-04-02 00:15:28 +0900

    헤더 목록 중앙정렬, 결제 페이지 카드 뒷면 색 추가, 텍스트 중앙정렬

--- src/introduce/pay.ts
+++ src/introduce/pay.ts
@@ -12,28 +12,20 @@ function checkboxToggle() {
     let current = null;
     for (let i = 0; i < priceContainer.length; i++) {
         priceContainer[i].addEventListener('click', function () {
-            if (current === null) {
+            if (current !== i) {
+                if (current !== null) { // 다른 곳이 이미 선택된 상태에서 또 다른 곳을 선택할 때 
+                    priceBox[current].style.transform = "perspective(1000px) rotateY(0deg)";
+                    priceAnchorBox[current].style.transform = "perspective(1200px) rotateY(180deg)";
+                    priceAnchorHoverBox[current].style.transform = "perspective(1000px) rotateY(0deg)";
+                    priceAnchorHoverBox[current].addEventListener('mouseover', overEventHandler[current]);
+                    priceAnchorHoverBox[current].addEventListener('mouseout', outEventHandler[current]);
+                } //처음 선택할때 + 다른 곳이 이미 선택된 상태에서 또 다른 곳을 선택할 때  
                 priceBox[i].style.transform = "perspective(1200px) rotateY(-179.9deg)";
                 priceAnchorBox[i].style.transform = "perspective(1200px) rotateY(0)";
                 priceAnchorHoverBox[i].removeEventListener('mouseover', overEventHandler[i]);
                 priceAnchorHoverBox[i].removeEventListener('mouseout', outEventHandler[i]);
                 priceAnchorHoverBox[i].style.transform = "perspective(1200px) rotateY(-179.9deg)";
                 priceAnchorHoverBox[i].style.opacity = "0";
-
-            } else {
-                if (current !== i) {
-                    priceBox[current].style.transform = "perspective(1000px) rotateY(0deg)";
-                    priceAnchorBox[current].style.transform = "perspective(1200px) rotateY(180deg)";
-                    priceAnchorHoverBox[current].style.transform = "perspective(1000px) rotateY(0deg)";
-                    priceAnchorHoverBox[current].addEventListener('mouseover', overEventHandler[current]);
-                    priceAnchorHoverBox[current].addEventListener('mouseout', outEventHandler[current]);
-                    priceAnchorHoverBox[i].removeEventListener('mouseover', overEventHandler[i]);
-                    priceAnchorHoverBox[i].removeEventListener('mouseout', outEventHandler[i]);
-                    priceBox[i].style.transform = "perspective(1200px) rotateY(-179.9deg)";
-                    priceAnchorBox[i].style.transform = "perspective(1200px) rotateY(0)";
-                    priceAnchorHoverBox[i].style.transform = "perspective(1200px) rotateY(-179.9deg)";
-                    priceAnchorHoverBox[i].style.opacity = "0";
-                }
             }
             current = i;
         });
