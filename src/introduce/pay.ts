66a999fa6a95bdb50057c48de9171a802caa6455
sso <sso9524@naver.com>
2020-04-01 14:53:51 +0900

    기본 툴 번호 체크기능 작업 중

--- src/introduce/pay.ts
+++ src/introduce/pay.ts
@@ -1,32 +1,60 @@
 const priceCheckbox = document.querySelectorAll<HTMLInputElement>('.price-checkbox');
 const priceContainer = document.querySelectorAll<HTMLElement>('.price-container');
+const cardHoverBox = document.querySelectorAll<HTMLElement>('.price-service-anchor-hover-box');
 
 checkboxToggle();
 
 function checkboxToggle() {
     let current = null;
+    const overEventHandler: (() => void)[] = [];
+    const outEventHandler: (() => void)[] = [];
     for (let i = 0; i < priceContainer.length; i++) {
         priceContainer[i].addEventListener('click', function () {
             if (current === null) {
                 priceContainer[i].classList.add("cardRotate");
+                cardHoverBox[i].removeEventListener('mouseover', overEventHandler[i]);
+                cardHoverBox[i].removeEventListener('mouseout', outEventHandler[i]);
+                cardHoverBox[i].style.opacity = "0";
             } else {
                 if (current !== i) {
                     priceContainer[current].classList.remove("cardRotate");
                     priceContainer[current].classList.add("backRotate");
                     priceContainer[i].classList.remove('backRotate');
                     priceContainer[i].classList.add("cardRotate");
+                    cardHoverBox[current].addEventListener('mouseover', overEventHandler[current]);
+                    cardHoverBox[current].addEventListener('mouseout', outEventHandler[current]);
+                    cardHoverBox[current].style.opacity = "0";
+                    cardHoverBox[i].removeEventListener('mouseover', overEventHandler[i]);
+                    cardHoverBox[i].removeEventListener('mouseout', outEventHandler[i]);
+                    cardHoverBox[i].style.opacity = "0";
                 } else {
                     if (priceContainer[i].classList.contains('cardRotate')) {
                         priceContainer[i].classList.add('backRotate');
                         priceContainer[i].classList.remove("cardRotate");
+                        cardHoverBox[i].addEventListener('mouseover', overEventHandler[i]);
+                        cardHoverBox[i].addEventListener('mouseout', outEventHandler[i]);
+                        cardHoverBox[i].style.opacity = "0";
                     } else {
                         priceContainer[i].classList.remove('backRotate');
                         priceContainer[i].classList.add("cardRotate");
+                        cardHoverBox[i].removeEventListener('mouseover', overEventHandler[i]);
+                        cardHoverBox[i].removeEventListener('mouseout', outEventHandler[i]);
                     }
                 }
             }
             current = i;
         });
+
+        const overEvent = () => {
+            cardHoverBox[i].style.opacity = "1";
+        }
+        overEventHandler.push(overEvent);
+        cardHoverBox[i].addEventListener('mouseover', overEvent);
+        const outEvent = () => {
+            cardHoverBox[i].style.opacity = "0";
+        }
+        outEventHandler.push(outEvent);
+        cardHoverBox[i].addEventListener('mouseout', outEvent)
     }
 }
 
--- src/system/premium/Layout/Layout3.ts
+++ src/system/premium/Layout/Layout3.ts
@@ -2,13 +2,19 @@ import { setColorLotto } from "../../../functions";
 
 export default class Layout3 {
     static makeLine(canvas: HTMLElement, numbers: number[]): void {
-        numbers.forEach(num => {
+        let current = null;
+        let flag = false;
+        numbers.forEach((num, index) => {
             const divBox = document.createElement('div');
             divBox.classList.add('func3-select-num-box');
 
             const div = document.createElement('div');
             div.classList.add('func3-select-num');
             div.textContent = num.toString();
+            div.addEventListener('click', () => {
+                flag = !flag;
+                current = num;
+            })
             setColorLotto(num, div);
 
             divBox.appendChild(div);
