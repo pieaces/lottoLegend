5cc652f6a495b0639483114e8e3f96696456f78f
sso <sso9524@naver.com>
2020-04-01 13:35:21 +0900

    결제 페이지 카드 디자인 수정

--- src/introduce/pay.ts
+++ src/introduce/pay.ts
@@ -5,53 +5,28 @@ checkboxToggle();
 
 function checkboxToggle() {
     let current = null;
-    const overEventHandler: (() => void)[] = [];
-    const outEventHandler: (() => void)[] = [];
-    for (let i = 0; i < priceCheckbox.length; i++) {
-        priceCheckbox[i].addEventListener('click', () => {
-            if (priceCheckbox[i].checked) {
-                if (current === null) {
-
-                } else {
-                    priceCheckbox[current].checked = false;
-                    priceContainer[current].style.transition = "transform 0.6s";
-                    priceContainer[current].style.transform = "none";
-                    ((priceCheckbox[current].parentNode as HTMLElement).previousElementSibling as HTMLElement).style.opacity = "1";
-                    priceContainer[current].addEventListener('mouseover', overEventHandler[current]);
-                    priceContainer[current].addEventListener('mouseout', outEventHandler[current]);
-                }
-                (priceCheckbox[i].parentNode.parentNode as HTMLElement).style.transition = "none";
-                ((priceCheckbox[i].parentNode as HTMLElement).previousElementSibling as HTMLElement).style.opacity = "0";
-
-                priceContainer[i].removeEventListener('mouseover', overEventHandler[i]);
-                priceContainer[i].removeEventListener('mouseout', outEventHandler[i]);
-
-                priceContainer[i].style.transform = "rotateY(180deg)";
-
-                current = i;
-            } else {
-                priceContainer[i].style.transition = "transform 0.6s";
-                ((priceCheckbox[i].parentNode as HTMLElement).previousElementSibling as HTMLElement).style.opacity = "1";
-                priceContainer[i].addEventListener('mouseover', overEventHandler[i]);
-                priceContainer[i].addEventListener('mouseout', outEventHandler[i]);
-                current = null;
-            }
-
-        })
-    }
-
     for (let i = 0; i < priceContainer.length; i++) {
-        const overEvent = () => {
-            priceContainer[i].style.transform = "rotateY(180deg)"
-        }
-        const outEvent = () => {
-            priceContainer[i].style.transform = "none";
-        }
-        overEventHandler.push(overEvent);
-        outEventHandler.push(outEvent);
-        priceContainer[i].addEventListener('mouseover', overEvent);
-        priceContainer[i].addEventListener('mouseout', outEvent);
+        priceContainer[i].addEventListener('click', function () {
+            if (current === null) {
+                priceContainer[i].classList.add("cardRotate");
+            } else {
+                if (current !== i) {
+                    priceContainer[current].classList.remove("cardRotate");
+                    priceContainer[current].classList.add("backRotate");
+                    priceContainer[i].classList.remove('backRotate');
+                    priceContainer[i].classList.add("cardRotate");
+                } else {
+                    if (priceContainer[i].classList.contains('cardRotate')) {
+                        priceContainer[i].classList.add('backRotate');
+                        priceContainer[i].classList.remove("cardRotate");
+                    } else {
+                        priceContainer[i].classList.remove('backRotate');
+                        priceContainer[i].classList.add("cardRotate");
+                    }
+                }
+            }
+            current = i;
+        });
     }
-
 }
 
