3ac1fd70a60f530d6015589e5352d408b12b9446
sso <sso9524@naver.com>
2020-04-02 16:59:07 +0900

    카드 디자인 수정

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { headerSign, isLogedIn } from './amplify/auth';
 configure();
 headerSign();
 
+const imgBtn = document.querySelectorAll('.img-btn > i');
 document.querySelector<HTMLElement>('.login-btn').onclick = () => {
 
 }
