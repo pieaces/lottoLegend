71af5739ebd4c92c52f5f1a61064af06d0cf51db
pieaces <jaesung446@gmai.com>
2020-04-04 22:40:15 +0900

    딱한번만

--- API/index.ts
+++ API/index.ts
@@ -1,17 +1,11 @@
 import autoPutSecond from "./dynamo/autoPutSecond";
 import autoDelete from "./dynamo/autoDelete";
 import autoRank from "./dynamo/autoRank";
-import counterLotto, { getCurrentRound } from "./dynamo/functions";
 
 exports.handler = async () => {
-    const total = getCurrentRound();
-    const count = await counterLotto() - 1;
-    if (count < total) {
-        await autoPutSecond();
-        await autoDelete();
-        await autoRank();
-    }else{
-        console.log('최신화 상태');
-    }
+    await autoPutSecond();
+    await autoDelete();
+    await autoRank();
+
     console.log('데이터 자동화 시스템 종료');
 };
