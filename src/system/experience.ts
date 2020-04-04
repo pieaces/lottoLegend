86433c6445a836a882b9500edc5b8d1cffea13f9
pieaces <jaesung446@gmai.com>
2020-04-04 19:29:26 +0900

    버블차트 옆 도표

--- src/system/experience.ts
+++ src/system/experience.ts
@@ -1,11 +1,18 @@
 import configure from '../amplify/configure'
 import DataAPI from "./experience/index";
 import Layout, { IDataAPI } from './premium/Layout'
+import Swal from 'sweetalert2';
 
 configure();
 
 const loading = document.querySelector<HTMLElement>('.loading-box');
 loading.classList.remove('none');
+Swal.fire({
+    title:'체험하기 설명',
+    text:'베르누이 분석툴의 기능을 체험할 수 있습니다.',
+    icon:'info'
+});
+document.getElementById('swal2-content').innerHTML = '베르누이 분석툴의 기능을 체험할 수 있습니다.<br>단, 모든 데이터는 과거의 데이터입니다.<br><span style="font-weight:bold;">기능만 체험해주세요.</span>'
 const layout = new Layout(DataAPI.getInstance() as IDataAPI);
 layout.init();
 loading.classList.add('none');
--- src/system/premium/Chart/bubble.js
+++ src/system/premium/Chart/bubble.js
@@ -12,6 +12,8 @@ export default class BubbleChart {
 
         const statsData = this.dataAPI.getStats();
         const labels = this.dataAPI.getLabels();
+        const X = [];
+        const Y = [];
         for (let i = 0; i < labels.length; i++) {
             let x = statsData.ideal['all'][i] - statsData.actual['all'][i];
             if (statsData.ideal['all'][i] >= statsData.actual['all'][i]) x /= statsData.ideal['all'][i];
@@ -22,9 +24,34 @@ export default class BubbleChart {
             else y /= statsData.actual['latest'][i];
             y *= statsData.pos[i];
 
+            X.push(x), Y.push(y);
             const data = [labels[i].toString(), x, y, Math.pow(2, x) * Math.pow(2, y), statsData.pos[i]];
             dataBubble.push(data);
         }
+        let xmin = { value: X[0], index: 0 }, xmax = { value: X[0], index: 0 }, ymin = { value: Y[0], index: 0 }, ymax = { value: Y[0], index: 0 };
+        for (let i = 1; i < labels.length; i++) {
+            if (xmin.value > X[i]) {
+                xmin.value = X[i];
+                xmin.index = i;
+            }
+            if (ymin.value > Y[i]) {
+                ymin.value = Y[i];
+                ymin.index = i;
+            }
+            if (xmax.value < X[i]) {
+                xmax.value = X[i];
+                xmax.index = i;
+            }
+            if (ymax.value < Y[i]) {
+                ymax.value = Y[i];
+                ymax.index = i;
+            }
+        }
+        document.getElementById('func1-bubble-all-most-value').textContent = xmin.index;
+        document.getElementById('func1-bubble-all-less-value').textContent = xmax.index;
+        document.getElementById('func1-bubble-five-most-value').textContent = ymin.index;
+        document.getElementById('func1-bubble-five-less-value').textContent = ymax.index;
+
         const dataTable = google.visualization.arrayToDataTable(dataBubble);
         const option = {
             bubble: { textStyle: { fontSize: 11 } },
