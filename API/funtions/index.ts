79946156b99c3b9aa2371527b08fbff32954c059
pieaces <jaesung446@gmai.com>
2020-04-08 12:31:21 +0900

    getcurrent 시간 55

--- API/funtions/index.ts
+++ API/funtions/index.ts
@@ -1,7 +1,7 @@
 import Response from '../Response'
 
 export function getCurrentRound(currentDate?: string): number {
-    const theDate = new Date('2020-02-01:11:51');
+    const theDate = new Date('2020-02-01:11:55');
     const today = currentDate && new Date(currentDate) || new Date();
     const between = Number(today) - Number(theDate);
     const plusDate = Math.floor(between / 24 / 3600 / 1000 / 7);
