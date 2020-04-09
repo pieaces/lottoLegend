23c8a3e68014358ea3ed3e506cf150a431ef2ea8
pieaces <jaesung446@gmai.com>
2020-04-09 13:46:52 +0900

    완료

--- API/index.ts
+++ API/index.ts
@@ -1,6 +1,11 @@
 import { scanUsers } from "./dynamodb/functions";
+import { getCurrentRound } from "./functions";
+import publish from "./sns/publish";
 
-exports.handler = (event: any, context: any, callback: any) => {
-    
-    scanUsers()
+exports.handler = async (event: any, context: any, callback: any) => {
+    const round = getCurrentRound();
+    const users = await scanUsers(round);
+    for(let i =0; i<users.length; i++){
+        await publish(JSON.stringify(users[i].numbers), users[i].phone);
+    }
 };
--- API/sns/publish.ts
+++ API/sns/publish.ts
@@ -1,14 +1,14 @@
-const AWS = require('aws-sdk');
+import AWS from 'aws-sdk';
 
 const sns = new AWS.SNS({ region: 'ap-northeast-1' });
 
-export default function publish(message, PhoneNumber) {
+export default function publish(message: string, PhoneNumber: string):Promise<void> {
     const params = {
         Message: message,
         PhoneNumber
     };
     return new Promise((resolve, reject) => {
-        sns.publish(params, function (err, data) {
+        sns.publish(params, function (err: any, data: any) {
             if (err) reject(err);
             resolve();
         });
