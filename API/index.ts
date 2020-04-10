dd7e41e6b276ba1d2d2e5d81e488aa2687f046f6
pieaces <jaesung446@gmai.com>
2020-04-11 00:19:34 +0900

    stats/piece

--- API/index.ts
+++ API/index.ts
@@ -97,10 +97,14 @@ exports.handler = async (event: any) => {
                 }
             }
             break;
-        case '/stats/piece':
+        case '/stats/piece': {
+            let ProjectionExpression:string;
+            ProjectionExpression = 'Ideal.#12,Ideal.#24,Ideal.#48,Ideal.#192,Ideal.#all, Ideal.latest12, Actual.#12,Actual.#24,Actual.#48,Actual.#192,Actual.#all,Actual.latest12, Pos, Stats, Piece';
+            const ExpressionAttributeNames = { '#12': '$12', '#24': '$24', '#48': '$48', '#192': '$192', '#all': 'all' };
             body = await queryStats(method as StatsMethod,
-                'Ideal.#12,Ideal.#24,Ideal.#48,Ideal.#192,Ideal.#all, Ideal.latest12, Actual.#12,Actual.#24,Actual.#48,Actual.#192,Actual.#all,Actual.latest12, Pos, Stats, Piece',
-                {'#12':'$12','#24':'$24','#48':'$48','#192':'$192', '#all':'all'});
+                ProjectionExpression,
+                ExpressionAttributeNames);
+        }
     }
     const response = {
         statusCode,
