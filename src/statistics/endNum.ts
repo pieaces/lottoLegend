e4cf8c76010c385359978721382dfbe56356a522
신상옥 <sso9524@naver.com>
2020-04-11 21:46:42 +0900

    로또 표 작성

---
+++ src/statistics/endNum.ts
@@ -0,0 +1,11 @@
+const roundEl=document.querySelectorAll('.lotto-num-box tr > td:nth-child(1)');
+
+const numsEl=document.querySelectorAll('.lotto-num-box tr > td:nth-child(n+2):last-child');
+
+roundEl.forEach(item=>{
+
+});
+
+numsEl.forEach(item=>{
+
+});
