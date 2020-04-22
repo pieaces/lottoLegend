ff55ef9339c21a0fe48b2c0ac922965c4c1619f7
EC2 Default User <ec2-user@ip-172-16-0-6.ap-northeast-2.compute.internal>
2020-04-22 07:00:20 +0000

    sh file add

---
+++ logfile.sh
@@ -0,0 +1,4 @@
+sudo touch /var/www/log/generate_cron.log
+sudo touch /var/www/log/distribute_cron.log
+sudo chmod 764 /var/www/log/generate_cron.log
+sudo chmod 764 /var/www/log/distribute_cron.log
