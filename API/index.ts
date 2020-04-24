dd0a19ae2c87d3a56a20adee12e3a0a5fca0aba7
pieaces <jaesung446@gmai.com>
2020-04-24 16:28:36 +0900

    numbers header

--- API/index.ts
+++ API/index.ts
@@ -8,7 +8,8 @@ import { getLottoData, getWinStats } from "./dynamoDB/getMainPage";
 
 const headers = {
     "Access-Control-Allow-Origin": "*", // Required for CORS support to work
-    //"Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
+    "Access-Control-Allow-Credentials" : true, // Required for cookies, authorization headers with HTTPS
+    "Cache-Control": "max-age=3"
 }
 exports.handler = async (event: any) => {
     if (event['detail-type'] === 'Scheduled Event') {
