e59992815462295ba3fccf46a988240dcdb1b6b7
pieaces <jaesung446@gmai.com>
2020-05-18 00:55:43 +0900

    aligo 적용

--- logfile.sh
+++ logfile.sh
@@ -1,4 +1,6 @@
+sudo rm /var/www/log/generate_cron.log
+sudo rm /var/www/log/distribute_cron.log
 sudo touch /var/www/log/generate_cron.log
 sudo touch /var/www/log/distribute_cron.log
-sudo chmod 764 /var/www/log/generate_cron.log
-sudo chmod 764 /var/www/log/distribute_cron.log
+sudo chmod 777 /var/www/log/generate_cron.log
+sudo chmod 777 /var/www/log/distribute_cron.log
--- src/sns/publish.ts
+++ src/sns/publish.ts
@@ -1,19 +1,31 @@
-import AWS, { AWSError } from 'aws-sdk';
+import { AWSError } from 'aws-sdk';
 import { getCurrentRound, numsArrToString } from '../funtions';
 import { GetItemInput } from 'aws-sdk/clients/dynamodb';
 import { dynamoDB } from '../dynamoDB';
 
-const sns = new AWS.SNS({ region: 'ap-northeast-1' });
-
-export default function publish(message: string, PhoneNumber: string): Promise<void> {
+//
+import querystring from 'querystring';
+import request from 'request';
+//
+export default function publish(message: string, phoneNumber: string, title="로또끝"): Promise<void> {
     const params = {
-        Message: message,
-        PhoneNumber
+        "key": "46srm39zvo8hbsv9rkrk3z67ja56p5wc",
+        "user_id": "lottoend",
+        "sender": "15991707",
+        "receiver": '0' + phoneNumber.slice(3),
+        "msg": message,
+        "title": title,
+        "msg_type": "MMS",
     };
+
     return new Promise((resolve, reject) => {
-        sns.publish(params, function (err: any, data: any) {
-            if (err) reject(err);
-            resolve();
+        request.post({
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }, // important to interect with PHP
+            url: 'https://apis.aligo.in/send/',
+            body: querystring.stringify(params),
+        }, function (error, response, body) {
+            if(error) reject(error);
+            else resolve(body);
         });
     })
 }
@@ -21,9 +33,9 @@ export default function publish(message: string, PhoneNumber: string): Promise<v
 export async function putNumberMessage(nickName: string, planValue:number, numbersList: number[][], phone:string) {
     const round = getCurrentRound() + 1;
     if(nickName === ''){
-        await publish(`[로또끝] 함께해주셔서 감사합니다!\n${round}회 프리미엄 ${planValue}조합\n` + numsArrToString(numbersList), phone);
+        await publish(`[로또끝] 함께해주셔서 감사합니다!\n${round}회 프리미엄 ${planValue}조합\n` + numsArrToString(numbersList), phone, '로또끝 프리미엄 결제 완료');
     }else{
-        await publish(`[로또끝] ${nickName}님의 당첨을 기원합니다!\n${round}회 프리미엄 ${planValue}조합\n` + numsArrToString(numbersList), phone);
+        await publish(`[로또끝] ${nickName}님의 당첨을 기원합니다!\n${round}회 프리미엄 ${planValue}조합\n` + numsArrToString(numbersList), phone, '로또끝 프리미엄 조합');
     }
 };
 
