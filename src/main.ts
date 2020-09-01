efb37aa3cb775e6274bf991bc37e73c46a8886e0
pieaces <jaesung446@gmai.com>
2020-09-01 17:54:56 +0900

    로그인시 공지사항 안뜨느 오류 수정

--- src/main.ts
+++ src/main.ts
@@ -24,9 +24,9 @@ isLogedIn().then((result) => {
                 document.querySelector('#service').textContent = data.user.plan;
             }
             executeMakingBoard(data);
-            // if (!mqMobile.matches) {
-            //     makeScroll(data.notice);
-            // }
+            if (!mqMobile.matches) {
+                makeScroll(data.notice);
+            }
         }).catch(() => {networkAlert();});
     } else {
         loginAfterBox.classList.add('none');
